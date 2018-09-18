f019f2d4881a4a65d44ad123f037fb874daff730
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 10:07:05 +0600

    exception handling

--- Exception Handing/Class exceptions/derived_class_exception.cpp
+++ Exception Handing/Class exceptions/derived_class_exception.cpp
@@ -4,3 +4,27 @@
 	If we put base class first then the derived class catch block will never be reached.
 */
 
+
+#include<iostream> 
+using namespace std; 
+  
+class Base {}; 
+class Derived: public Base {}; 
+int main() 
+{ 
+   Derived d; 
+   // some other stuff 
+   try { 
+       // Some monitored code 
+       throw d; 
+   } 
+   catch(Base b) {  
+        cout<<"Caught Base Exception"; 
+   } 
+   catch(Derived d) {  //This catch block is NEVER executed 
+        cout<<"Caught Derived Exception"; 
+   } 
+   
+   return 0; 
+} 
+
