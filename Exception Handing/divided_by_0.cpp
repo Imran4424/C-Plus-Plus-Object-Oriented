90d16eb44d5d0a748ad0fa910abb969ac593eada
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 06:16:21 +0600

    exception handling

--- Exception Handing/divided_by_0.cpp
+++ Exception Handing/divided_by_0.cpp
@@ -5,5 +5,17 @@ int main(int argc, char const *argv[])
 {
 	int num, num2;
 
+	try
+	{
+		cout<<"enter two integer numbers respectively"<<endl;
+
+		cin>>num>>num2;
+
+		if (/* condition */)
+		{
+			/* code */
+		}
+	}
+
 	return 0;
 }
