3af4d265159255ac1c6887b45a338835cac93a1b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 09:47:23 +0600

    exception handling

---
+++ Exception Handing/catch_all.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    try  { 
+       throw 10; 
+    } 
+    catch (char *excp)  { 
+        cout << "Caught " << excp; 
+    } 
+    catch (...)  { 
+        cout << "Default Exception\n"; 
+    } 
+    return 0; 
+}
