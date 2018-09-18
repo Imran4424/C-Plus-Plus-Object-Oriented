36ec36572dd90b83e6b663128c62bea4eaf02a5c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 12:08:44 +0600

    exception handling

--- Exception Handing/Nested/nested_exception.cpp
+++ Exception Handing/Nested/nested_exception.cpp
@@ -2,6 +2,9 @@
     this is a nested try catch block example
 
     we can rethrow an exception using "throw"
+
+    A function can also re-throw a function using same “throw; “.
+    A function can handle a part and can ask the caller to handle remaining.
 */
 
 #include <iostream> 
