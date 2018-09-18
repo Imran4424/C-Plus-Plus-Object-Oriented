f10d92a85fc1facac5759e5736e334a205438b19
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 10:45:24 +0600

    exception handling

--- Exception Handing/Type Convertion/no_type_conversion.cpp
+++ Exception Handing/Type Convertion/no_type_conversion.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	
+	try
+	{
+		throw 'x';
+	}
+	catch(int x)
+	{
+		cout<< "caught int x: "<< x << endl;
+	}
+	catch(...)
+	{
+		
+	}
+
 	return 0;
 }
