f68e4a69dac43ffbedd8240bf74d6ecd29b95137
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-18 11:05:40 +0600

    exception handling

---
+++ Exception Handing/Type Convertion/conversion_constructor_not_called.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream> 
+using namespace std; 
+  
+class MyExcept1 {}; 
+  
+class MyExcept2 
+{ 
+public: 
+  
+    // Conversion constructor 
+    MyExcept2 (const MyExcept1 &e ) 
+    { 
+        cout << "Conversion constructor called"; 
+    } 
+}; 
+  
+int main() 
+{ 
+    try
+    { 
+        MyExcept1 myexp1; 
+        throw myexp1; 
+    } 
+    catch(MyExcept2 e2) 
+    { 
+        cout << "Caught MyExcept2 " << endl; 
+    } 
+    catch(...) 
+    { 
+        cout << " Defaule catch block " << endl; 
+    } 
+    return 0; 
+} 
