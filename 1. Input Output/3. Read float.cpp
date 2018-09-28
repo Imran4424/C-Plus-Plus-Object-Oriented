e71337df95d15048495625b2d8f2dae208748407
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-09-28 18:44:49 +0600

    input output

--- 1. Input Output/3. Read float.cpp
+++ 1. Input Output/3. Read float.cpp
@@ -30,4 +30,6 @@ int main(int argc, char const *argv[])
 	if input value is 0 precision output will be 0 precision
 	if input value is 1 precision output will be 1 precision
 	if input value is 2 precision output will be 2 precision
+
+	and so on
 */
