2e42b6ca137a0f63f794d9970f40195e49f9b5ec
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-10-03 06:47:54 +0600

    Input Output

--- 1. Input Output/1.Basic_Ouptput.cpp
+++ 1. Input Output/1.Basic_Ouptput.cpp
@@ -18,6 +18,9 @@ int main(int argc, char const *argv[])
 
 	iostream holds cin cout
 
+	cin : for input stream
+	cout : for output stream
+
 	int main(int argc, char const *argv[]); this is for console input
 
 	if you don't use console input you can skip these parameters
