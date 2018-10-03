7d975c732cc8448174663c675501e3ea6adb3811
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-10-03 06:35:31 +0600

    Input Output

--- 1. Input Output/2.Variable_display.cpp
+++ 1. Input Output/2.Variable_display.cpp
@@ -1,3 +1,12 @@
 /*
 	An variable num contains 7. Write a program that prints the value of num.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
