26f20938e81c4fbd2a1a55a8252e9c6e4db8532b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 19:45:52 +0600

    c++ input output

--- 1. Input Output/8.ASCII_char.cpp
+++ 1. Input Output/8.ASCII_char.cpp
@@ -2,3 +2,18 @@
 	Write a program that read any ASCII value and display equivalent character
 */
 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int num;
+
+	cin>>num;
+
+	char ch = (char) num; // type casting
+
+	cout<<"Entered ASCII value equivalent character is: "<<ch<<endl;
+	
+	return 0;
+}
