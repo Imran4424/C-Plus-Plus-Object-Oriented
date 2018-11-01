3b89f8fd6536ad9fa6cf70f293db4d0a92d03d48
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 19:30:53 +0600

    c++ input output

--- 1. Input Output/7. Read char.cpp
+++ 1. Input Output/7. Read char.cpp
@@ -1,3 +1,17 @@
 /*
 	Write a program that read and display any character
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char ch;
+
+	cin>>ch;
+
+	cout<<"The entered character is: "<<ch<<endl;
+	
+	return 0;
+}
