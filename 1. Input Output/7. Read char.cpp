aa6bab4f5c72cd568369222f022367e077e50b82
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 19:32:31 +0600

    c++ input output

--- 1. Input Output/7. Read char.cpp
+++ 1. Input Output/7. Read char.cpp
@@ -12,6 +12,12 @@ int main(int argc, char const *argv[])
 	cin>>ch;
 
 	cout<<"The entered character is: "<<ch<<endl;
-	
+
 	return 0;
 }
+
+/*
+	for single character input,
+
+	cin is really good input stream
+*/
