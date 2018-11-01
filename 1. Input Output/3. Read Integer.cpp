a3bab2f4b960060f2fd8e9487f9791fe25b405cd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 19:15:31 +0600

    c++ input output

--- 1. Input Output/3. Read Integer.cpp
+++ 1. Input Output/3. Read Integer.cpp
@@ -2,7 +2,7 @@
 	Write a program that read a integer number and display it
 */
 
-#include <iostream> // for cin 
+#include <iostream> // for cin, cout
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -15,3 +15,18 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	In C++,
+
+		cin  : for input stream
+		cout : for output stream
+
+	In C coding, we always have to remember format specifier for different data types
+
+	Here, C++ gives us a very good simpliciy
+
+	we don't have to remember format spceifier anymore for different types of varibale
+
+	using cin we can take input any types of variable without knowing format specifier
+*/
