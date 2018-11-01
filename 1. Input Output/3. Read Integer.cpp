2c90ddcb2774cead5d40e2baed86e57be2156922
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 18:41:11 +0600

    c++ input output

--- 1. Input Output/3. Read Integer.cpp
+++ 1. Input Output/3. Read Integer.cpp
@@ -1,3 +1,17 @@
 /*
 	Write a program that read a integer number and display it
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int num;
+
+	 cin>>num;
+
+	 cout<<"The entered int number is: "<<num<<endl;
+
+	return 0;
+}
