51ec1b8b4c80d8897d15cd47e719a0401422cfed
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 20:19:42 +0600

    c++ input output

---
+++ 1. Input Output/10.display_name.cpp
@@ -0,0 +1,18 @@
+/*
+	Write a program that read and display name;
+*/
+
+
+int main(int argc, char const *argv[])
+{
+	char name[25];
+
+	cin>>name;
+
+	cout<<"The entered name is: "<<name<<endl;
+	return 0;
+}
+
+/*
+
+*/
