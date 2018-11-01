601c462d89c79b85d078533e7833f0712f319425
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-01 19:48:06 +0600

    c++ input output

--- 1. Input Output/8.ASCII_char.cpp
+++ 1. Input Output/8.ASCII_char.cpp
@@ -14,6 +14,13 @@ int main(int argc, char const *argv[])
 	char ch = (char) num; // type casting
 
 	cout<<"Entered ASCII value equivalent character is: "<<ch<<endl;
-	
+
 	return 0;
 }
+
+/*
+	Here, 
+		we reading a ASCII value in int(decimal) format using cin
+
+		them displaying the equivalent character using cout after type casting
+*/
