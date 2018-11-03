c5f4ebdb544ce2686922be3b2135c9a42b7346c5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 12:21:37 +0600

    c++ input output

--- 1. Input Output/2. IO members/9.Bool_Input_output.cpp
+++ 1. Input Output/2. IO members/9.Bool_Input_output.cpp
@@ -9,9 +9,21 @@ int main(int argc, char const *argv[])
 {
 	bool status;
 
+	cin.setf(ios::boolalpha); 
 	cin>>status;
 
+	cout.setf(ios::boolalpha);
 	cout<<status<<endl;
 
 	return 0;
 }
+
+/*
+	cin.setf(ios::boolalpha); 
+
+	for setting boolean input to true,false instead of 1,0
+
+	cout.setf(ios::boolalpha);
+	
+	for setting boolean output to true,false instead of 1,0
+*/
