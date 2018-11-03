dcd0898811a8ced0f235c64504f2c26153d20d85
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 12:03:34 +0600

    c++ input output

--- 1. Input Output/2. IO members/8.width_fill.cpp
+++ 1. Input Output/2. IO members/8.width_fill.cpp
@@ -21,7 +21,14 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	cout.width
+	cout.width(int)
 
-	
+	width occupied given number of character space for next next imediate output statement
+
+	cout.fill('.')
+
+	fill fills remaining unused character of occupied by width with a the given character 
+
+
+	width and fill only works on imediate output statement
 */
