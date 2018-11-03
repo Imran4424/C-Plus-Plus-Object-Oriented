c9cf5195caec12be5135cb9732092f43eea80f67
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 11:27:08 +0600

    c++ input output

--- 1. Input Output/2. IO members/6.Setting_multiple_flags.cpp
+++ 1. Input Output/2. IO members/6.Setting_multiple_flags.cpp
@@ -1,3 +1,19 @@
 /*
-	Write a program to read 
+	Write a program to setting multiple flags in single statement
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	double PI = 3.1416;
+
+	cout<<PI<<endl;
+
+	cout.setf(ios::showpos | ios::showpoint);
+
+	cout<<PI<<endl;
+
+	return 0;
+}
