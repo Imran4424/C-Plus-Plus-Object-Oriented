7729a01fcf435cc4fce8a961101d47bf44ce521a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 11:53:01 +0600

    c++ input output

--- 1. Input Output/2. IO members/8.width_fill.cpp
+++ 1. Input Output/2. IO members/8.width_fill.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	cout.width(25);
+	cout<<"I am a Programmer"<<endl;
 	
+	cout.width(25);
+	cout.fill(.);
+	cout<<"I am a Programmer"<<endl;
+
+	cout<<"I am a Programmer"<<endl;
+
 	return 0;
 }
