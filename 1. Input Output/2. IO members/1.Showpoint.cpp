8c83045c72c394d7f7845e136130d1c691a232b8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 09:40:01 +0600

    c++ input output

--- 1. Input Output/2. IO members/1.Showpoint.cpp
+++ 1. Input Output/2. IO members/1.Showpoint.cpp
@@ -9,10 +9,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	double a = 2.97;
-	double b = 56;
+	double b = 560000;
 	const double PI = 3.1416;
 
 	cout<<a<<"\t"<<b<<"\t"<<PI<<endl;
+
+	cout.setf(ios::showpoint);
+
+	cout<<a<<"\t"<<b<<"\t"<<PI<<endl;
 	
+
 	return 0;
 }
