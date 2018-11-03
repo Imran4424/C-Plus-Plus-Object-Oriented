22ddec99aa48ec7b9e268da7d5524b11f8b4d0ad
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 11:40:11 +0600

    c++ input output

--- 1. Input Output/2. IO members/7.setting_flag_without_setf.cpp
+++ 1. Input Output/2. IO members/7.setting_flag_without_setf.cpp
@@ -1,3 +1,13 @@
 /*
-	
+	Write a program that demonstrate how to set flags without setf
 */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
