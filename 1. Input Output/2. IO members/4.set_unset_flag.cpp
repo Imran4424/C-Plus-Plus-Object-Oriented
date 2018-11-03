a190902608a292f149b9adedbaa9d2b0c1b4c0b4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 10:56:45 +0600

    c++ input output

--- 1. Input Output/2. IO members/4.set_unset_flag.cpp
+++ 1. Input Output/2. IO members/4.set_unset_flag.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	int num = 100;
+
+	cout<<"Decimal: "<<num<<endl;
+
+	cout.setf(ios::hex);
+
+	cout<<"hexadecimal: "<<num<<endl;
 	return 0;
 }
