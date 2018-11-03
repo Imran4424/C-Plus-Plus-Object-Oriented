1da7fd04da3887864dd9d2d08b73673ae1e78943
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 11:07:48 +0600

    c++ input output

--- 1. Input Output/2. IO members/5.check_flag.cpp
+++ 1. Input Output/2. IO members/5.check_flag.cpp
@@ -2,3 +2,22 @@
 	Write a program to demonstrate checking whether it is set or not
 */
 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout<<"Decimal: "<<num<<endl;
+
+	cout.setf(ios::hex);
+
+	cout<<"hexadecimal: "<<num<<endl; // not working
+
+	cout.unsetf(ios::dec);
+	
+	cout<<"hexadecimal: "<<num<<endl; //working
+
+	
+
+	return 0;
+}
