854e168301080b02595b5c9249cb232f330579b2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 10:20:07 +0600

    c++ input output

--- 1. Input Output/2. IO members/3.uppercse_lowercse.cpp
+++ 1. Input Output/2. IO members/3.uppercse_lowercse.cpp
@@ -2,3 +2,14 @@
 	Write a program to explore ios::uppercse and ios::lowercase properties
 */
 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char text;
+
+	cin.getline(text, sizeof(text));
+	
+	return 0;
+}
