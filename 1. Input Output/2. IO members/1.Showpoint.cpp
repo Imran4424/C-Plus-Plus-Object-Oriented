60b6019fb3b0ef0e0dd65f667e4cbb193ba9c62f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 09:34:17 +0600

    c++ input output

--- 1. Input Output/2. IO members/1.Showpoint.cpp
+++ 1. Input Output/2. IO members/1.Showpoint.cpp
@@ -3,16 +3,12 @@
 */
 
 #include <iostream>
+using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
 
-	std::cout<<"Not using std namespace"<<std::endl;
-
-	using namespace std;
-
-	cout<<"I am a programmer"<<endl;
-
+	
 	return 0;
 }
