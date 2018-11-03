fc6685db86e44fe46ec84e210c673885f09d28f5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-03 12:17:33 +0600

    c++ input output

---
+++ 1. Input Output/2. IO members/9.Bool_Input_output.cpp
@@ -0,0 +1,17 @@
+/*
+	Write a program to demonstrate boolean Input and output
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	bool status;
+
+	cin>>status;
+
+	cout<<status<<endl;
+
+	return 0;
+}
