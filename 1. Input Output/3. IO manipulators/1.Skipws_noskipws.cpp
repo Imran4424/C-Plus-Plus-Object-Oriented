d6930dcaf8cb11f41f50e552773b57b769010ed2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 03:18:06 +0600

    Input output

--- 1. Input Output/3. IO manipulators/1.Skipws_noskipws.cpp
+++ 1. Input Output/3. IO manipulators/1.Skipws_noskipws.cpp
@@ -2,3 +2,12 @@
 	Write a program to demonstrate the I/O manipulators skipws, noskipws 
 */
 
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
