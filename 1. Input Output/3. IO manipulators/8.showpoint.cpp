28a04d7efb7ae8475f8e3af5da5f563504fe5558
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 10:35:05 +0600

    Input output

--- 1. Input Output/3. IO manipulators/8.showpoint.cpp
+++ 1. Input Output/3. IO manipulators/8.showpoint.cpp
@@ -2,3 +2,11 @@
 	write a program to demonstrate I/O manipulators showpoint and noshowpoint
 */
 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
