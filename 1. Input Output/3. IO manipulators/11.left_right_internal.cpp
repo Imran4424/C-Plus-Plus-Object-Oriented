64b3a3ce59380db1cea310d34f579d11fd535a7f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 12:24:17 +0600

    Input Output

--- 1. Input Output/3. IO manipulators/11.left_right_internal.cpp
+++ 1. Input Output/3. IO manipulators/11.left_right_internal.cpp
@@ -8,6 +8,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	cout<< setw(7) << -71 << endl;
+
+	cout<< left << setw(7) << -71 << endl;
+	
+	cout<< right << setw(7) << -71 << endl;
+	
+	cout<< internal << setw(7) << -71 << endl;
 	
 	return 0;
 }
