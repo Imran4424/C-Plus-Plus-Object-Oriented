30107211ff072ab65292a915a006b7633793be96
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 10:38:41 +0600

    Input output

--- 1. Input Output/3. IO manipulators/8.showpoint.cpp
+++ 1. Input Output/3. IO manipulators/8.showpoint.cpp
@@ -8,8 +8,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	double PI = 3.1416;
-	double a = 123456.7;
+	double a = 123456.720;
 
-	
+	cout<< PI <<"\t"<< a <<endl;
+
+	cout<< showpoint << PI <<"\t"<< a <<endl;
+
 	return 0;
 }
+
+/*
+	showpoint - shows us 6 digits in total
+*/
