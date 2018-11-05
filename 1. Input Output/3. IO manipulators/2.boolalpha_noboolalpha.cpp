d9ad3e30ce24abb9f7f2f9d422b75806231beade
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 06:36:49 +0600

    Input output

--- 1. Input Output/3. IO manipulators/2.boolalpha_noboolalpha.cpp
+++ 1. Input Output/3. IO manipulators/2.boolalpha_noboolalpha.cpp
@@ -12,6 +12,22 @@ int main(int argc, char const *argv[])
 	cin>> boolalpha >> status;
 
 	cout << boolalpha << status <<endl;
-	
+
 	return 0;
 }
+
+/*
+	boolalpha - for setting boolean input or output to true,false insted of 1,0
+
+	noboolalpha - for setting boolean input or output to 1,0 instead of true,false
+
+	noboolalpha is been set by default
+
+	these are the memebers of iomanip headers
+
+	sometimes we need to include the header like this,
+
+										#include <iomanip>
+
+	sometimes it work without including the header iomanip
+*/
