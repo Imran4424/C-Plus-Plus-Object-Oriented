959a6c66fd636249a292941413cc767e619b7580
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 12:05:06 +0600

    Input output

--- 1. Input Output/3. IO manipulators/11.left_right_internal.cpp
+++ 1. Input Output/3. IO manipulators/11.left_right_internal.cpp
@@ -1,3 +1,13 @@
 /*	
 	write a program to demonstrate I/O manipulators fixed and setprecision
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
