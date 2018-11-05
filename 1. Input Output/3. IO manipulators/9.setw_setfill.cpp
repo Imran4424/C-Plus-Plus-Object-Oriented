7df15f3e3ce1e6aa4700c90ebfee506e356409db
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 11:37:34 +0600

    Input output

--- 1. Input Output/3. IO manipulators/9.setw_setfill.cpp
+++ 1. Input Output/3. IO manipulators/9.setw_setfill.cpp
@@ -1,3 +1,14 @@
 /*	
 	write a program to demonstrate I/O manipulators setw and setfill
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
