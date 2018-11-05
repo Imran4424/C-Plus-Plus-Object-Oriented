e2f1c867384e92c0c6ea7b5967ce2dfa1379b936
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 11:52:57 +0600

    Input output

--- 1. Input Output/3. IO manipulators/10.fixed_set_precision.cpp
+++ 1. Input Output/3. IO manipulators/10.fixed_set_precision.cpp
@@ -1,3 +1,12 @@
 /*	
 	write a program to demonstrate I/O manipulators fixed and setprecision
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
