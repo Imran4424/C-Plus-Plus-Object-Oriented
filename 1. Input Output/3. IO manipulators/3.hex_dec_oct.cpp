f8ac1055babe8d56bace63f4a476b0b21e00bcb4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 06:50:08 +0600

    Input output

--- 1. Input Output/3. IO manipulators/3.hex_dec_oct.cpp
+++ 1. Input Output/3. IO manipulators/3.hex_dec_oct.cpp
@@ -1,3 +1,16 @@
 /*
 	write a program to demonstrate I/O manipulators hex, oct and dec
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	int num = 200;
+
+	
+	return 0;
+}
