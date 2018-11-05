7b1d407ed029f8e5802bde1ee28428f87e27ece1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 06:13:43 +0600

    Input output

--- 1. Input Output/3. IO manipulators/1.Skipws_noskipws.cpp
+++ 1. Input Output/3. IO manipulators/1.Skipws_noskipws.cpp
@@ -20,3 +20,17 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	skipws - skip all the whitespaces in the input stream like space enter
+
+	noskipws - this is turn off the skipws from the input stream, in that case space and enter also count as input
+
+	by default skipws is set for all compilers
+
+	we have to set noskipws when we need it
+
+	here, we used noskipws by enabling it first ny using noskipws in input steam
+
+		then again we used  skipws enabling that by using skipws in input stream
+*/
