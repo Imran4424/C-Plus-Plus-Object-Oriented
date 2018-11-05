25841a441559f92cf25d9db60c79c06d1b65d8ce
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 09:19:22 +0600

    Input output

--- 1. Input Output/3. IO manipulators/5.showbase_noshowbse.cpp
+++ 1. Input Output/3. IO manipulators/5.showbase_noshowbse.cpp
@@ -1,3 +1,23 @@
 /*
 	write a program to demonstrate I/O manipulators showbase, noshowbase 
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	int num = 200;
+
+	cout<<"Hexadecimal: ";
+	cout << hex << num <<endl<<endl;
+
+	cout<<"Decimal: ";
+	cout << dec << num <<endl;
+
+	cout << oct << num <<endl;
+
+	return 0;
+}
