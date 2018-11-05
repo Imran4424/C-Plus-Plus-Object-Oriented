cc805d9a1db2f7f7aad96725e201a772d06fca02
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 07:02:10 +0600

    Input output

--- 1. Input Output/3. IO manipulators/3.hex_dec_oct.cpp
+++ 1. Input Output/3. IO manipulators/3.hex_dec_oct.cpp
@@ -21,5 +21,21 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	
+	hex - to print numbers in hexadecimal base
+
+	oct - to print numbers in octal base
+
+	dec - to print numbers in decimal base
+
+	dec is active by default for input output stream
+
+	these are the memebers of iomanip headers
+
+	sometimes we need to include the header like this,
+
+										#include <iomanip>
+
+	sometimes it work without including the header iomanip
+
+	In update compilers don't need to include iomanip
 */
