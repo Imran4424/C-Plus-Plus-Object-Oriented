a101b9291a688f914292acbf07d8a7ecf45e9422
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 10:23:17 +0600

    Input output

---
+++ 1. Input Output/3. IO manipulators/7.showpos_noshowpos.cpp
@@ -0,0 +1,12 @@
+/*	
+	write a program to demonstrate I/O manipulators showpos and noshowpos
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
