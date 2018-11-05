0541da608128c425bca4f50e3923160498f8bf6f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 09:29:57 +0600

    Input output

--- 1. Input Output/3. IO manipulators/5.showbase_noshowbse.cpp
+++ 1. Input Output/3. IO manipulators/5.showbase_noshowbse.cpp
@@ -32,4 +32,14 @@ int main(int argc, char const *argv[])
 	noshowbase - to hide the base of the number system
 
 	by default noshowbase is activated
+
+	these are the memebers of iomanip headers
+
+	sometimes we need to include the header like this,
+
+										#include <iomanip>
+
+	sometimes it work without including the header iomanip
+
+	In update compilers don't need to include iomanip
 */
