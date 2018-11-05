29c5cdd44bfa4be3a74d195feaa92ad10c349d4c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-05 10:17:57 +0600

    Input output

--- 1. Input Output/3. IO manipulators/6.uppercase_nouppercase.cpp
+++ 1. Input Output/3. IO manipulators/6.uppercase_nouppercase.cpp
@@ -1,3 +1,13 @@
 /*
-	
+	write a program to demonstrate I/O manipulators uppercase and nouppercase
 */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
