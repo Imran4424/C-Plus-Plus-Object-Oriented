c395a00b380cf5fef299d1d6c6b946bcc0616345
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-10 22:55:00 +0600

    c++ string class

--- 7. C++ String Class/1. string/1.initialization.cpp
+++ 7. C++ String Class/1. string/1.initialization.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	stirng str = "I am a Programmer";
+
+	cout<< "str" << endl;
+	return 0;
+}
