9a0941fc1f8685a0d79e08630004a59b5d924608
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 16:26:46 +0600

    c++ string class

--- 7. C++ String Class/1. string/12.push_back.cpp
+++ 7. C++ String Class/1. string/12.push_back.cpp
@@ -9,7 +9,21 @@ int main(int argc, char const *argv[])
 
 	status += ' ';
 
-	status 
+	status += 'p';
+
+	status.push_back('l');
+	status.push_back('a');
+	status.push_back('y');
+	status.push_back(' ');
+
+	status += 'g';
+	status += 'a';
+	status.push_back('m');
+	status.push_back('e');
+	status.push_back('s');
+
+
+	cout << "final string: " << status << endl;
 
 	return 0;
 }
