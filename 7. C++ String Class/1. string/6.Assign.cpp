85f429543896ea1f3869bf68ebb30d54dbb0f9e3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 13:22:30 +0600

    c++ string class

--- 7. C++ String Class/1. string/6.Assign.cpp
+++ 7. C++ String Class/1. string/6.Assign.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	string str, copystr;
+
+	getline(cin, str);
+
+	copystr.assign(str);
+
+	cout << "Input string : " << str << endl;
+
+	cout << "copy string : " << copyStr << endl;
+
 	return 0;
 }
