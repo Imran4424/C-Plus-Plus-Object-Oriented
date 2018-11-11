21d87537d30badb6bc5d737feca2ba5497ba02d9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 16:12:07 +0600

    c++ string class

--- 7. C++ String Class/1. string/10.Append_3.cpp
+++ 7. C++ String Class/1. string/10.Append_3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	string status = "I love to play";
-	string job = "I creates games";
+	string job = "I create games";
 
 	cout << endl << "Before append" << endl << endl;
 
