7a4cf9cd81b7a4a8d7635bc284ef110320ad9088
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-12 00:00:10 +0600

    c++ string class

--- 7. C++ String Class/1. string/14.Insert_2.cpp
+++ 7. C++ String Class/1. string/14.Insert_2.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	string status = "I Play games";
-	string element ="love to do programming";
+	string element ="I love to do programming";
 
-	status.insert(2, element, 8);
+	status.insert(2, element, 2, 8);
 
 	cout << "final string: " << status << endl;
 	return 0;
