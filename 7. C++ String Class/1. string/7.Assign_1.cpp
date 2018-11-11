a0596637acb9671236bfa9a845c1362a7a0677b3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 13:38:39 +0600

    c++ string class

--- 7. C++ String Class/1. string/7.Assign_1.cpp
+++ 7. C++ String Class/1. string/7.Assign_1.cpp
@@ -7,8 +7,11 @@ int main(int argc, char const *argv[])
 	string status = "I love to play games";
 	string command;
 
-	
+	command.assign(status, 3, status.size() - 1);
 
+	cout<< status << endl;
+
+	cout << command << endl;
 
 	return 0;
 }
