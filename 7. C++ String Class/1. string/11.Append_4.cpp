555a25718e0c6e94c12c871917fc732bca866571
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 16:18:50 +0600

    c++ string class

--- 7. C++ String Class/1. string/11.Append_4.cpp
+++ 7. C++ String Class/1. string/11.Append_4.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	string gibrish = "aa";
+
+	gibrish.append('2', 'b');
+
+	gibrish.append('4', 'c');
+
+	gibrish.append('2', 'd');
+
+	gibrish.append('3', 'e');
+
 	return 0;
 }
