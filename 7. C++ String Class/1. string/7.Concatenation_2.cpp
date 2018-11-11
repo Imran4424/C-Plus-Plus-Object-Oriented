ff4261013897feb01685ce8b1cfbf7ec4dc2039c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-11 07:46:47 +0600

    c++ string class

--- 7. C++ String Class/1. string/7.Concatenation_2.cpp
+++ 7. C++ String Class/1. string/7.Concatenation_2.cpp
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
 
 	string final2 = first + " Play " + "Games";
 
-	string final3 = "I" + play + " Games";
+	string final3 = "I " + "love to " + play + " Games";
 
 	cout<< "final string: " << final << endl;
 
