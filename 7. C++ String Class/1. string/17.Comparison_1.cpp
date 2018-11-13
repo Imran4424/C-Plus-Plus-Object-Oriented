884037e3ac9b2b74c06f75506ade152301600c0d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-13 07:07:33 +0600

    c++ string

--- 7. C++ String Class/1. string/17.Comparison_1.cpp
+++ 7. C++ String Class/1. string/17.Comparison_1.cpp
@@ -10,5 +10,18 @@ int main(int argc, char const *argv[])
 	getline(cin, str);
 	getline(cin, str2);
 
+	if (str > str2)
+	{
+		cout << str << " is greater than " << str2 << endl;
+	}
+	else if(str < str2)
+	{
+		cout << str << " is less than " << str2 << endl;
+	}
+	else
+	{
+		cout << str << " is equal to " << str2 << endl;
+	}
+
 	return 0;
 }
