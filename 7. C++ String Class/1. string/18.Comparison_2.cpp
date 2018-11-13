0cdc4f5330979e36efa037f203340d59e7a0cac7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-13 07:21:25 +0600

    c++ string

--- 7. C++ String Class/1. string/18.Comparison_2.cpp
+++ 7. C++ String Class/1. string/18.Comparison_2.cpp
@@ -12,6 +12,15 @@ int main(int argc, char const *argv[])
 	getline(cin, str2);
 
 	int status = str.compare(str2);
-	
+
+	if (status == 0)
+	{
+		cout << str << " is equal to " << str2 << endl;
+	}
+	else if (status > 0)
+	{
+		/* code */
+	}
+
 	return 0;
 }
