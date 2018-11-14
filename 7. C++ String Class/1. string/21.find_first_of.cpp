f60a84ac866c5ab7bd12d55686cb3cd6093fc315
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 07:26:25 +0600

    c++ string

--- 7. C++ String Class/1. string/21.find_first_of.cpp
+++ 7. C++ String Class/1. string/21.find_first_of.cpp
@@ -10,6 +10,14 @@ int main(int argc, char const *argv[])
 	string identifier = "Hope7";
 
 	int pos = identifier.find_first_of(digits);
-	
+
+	if (pos >= 0)
+	{
+		cout << " illegal identifier " << pos << endl;
+	}
+	else
+	{
+		cout << " legal identifier " << endl;
+	}
 	return 0;
 }
