e8c63ddab821505d169b573e2623b9a35cbf525b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 08:43:23 +0600

    c++ string

--- 7. C++ String Class/1. string/22.find_first_not_of.cpp
+++ 7. C++ String Class/1. string/22.find_first_not_of.cpp
@@ -5,7 +5,33 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	string digits = "0123456789";
+
+	string identifier = "Hope7";
+
+	int pos = identifier.find_first_not_of(digits);
+
+	if (pos >= 0)
+	{
+		cout << " illegal identifier " << pos << endl;
+	}
+	else
+	{
+		cout << " legal identifier " << endl;
+	}
+
+	identifier = "11dead";
+
+	pos = identifier.find_first_not_of(digits);
+
+	if (pos >= 0)
+	{
+		cout << " illegal identifier " << pos << endl;
+	}
+	else
+	{
+		cout << " legal identifier " << endl;
+	}
 
 	return 0;
 }
