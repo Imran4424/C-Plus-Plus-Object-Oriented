42f5c83b88ef20b997d71debf300e4a9a0c6f3b7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 07:29:18 +0600

    c++ string

--- 7. C++ String Class/1. string/21.find_first_of.cpp
+++ 7. C++ String Class/1. string/21.find_first_of.cpp
@@ -20,6 +20,18 @@ int main(int argc, char const *argv[])
 		cout << " legal identifier " << endl;
 	}
 
-	
+	identifier = "dead7man";
+
+	pos = identifier.find_first_of(digits);
+
+	if (pos >= 0)
+	{
+		cout << " illegal identifier " << pos << endl;
+	}
+	else
+	{
+		cout << " legal identifier " << endl;
+	}
+
 	return 0;
 }
