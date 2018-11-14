1ebf2f5537fead5d51f41491ca539a6c2cf69bf9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 08:54:37 +0600

    c++ string

--- 7. C++ String Class/1. string/23.find_last_of.cpp
+++ 7. C++ String Class/1. string/23.find_last_of.cpp
@@ -8,5 +8,9 @@ int main(int argc, char const *argv[])
 	string digits = "0123456789";
 
 	string identifier = "who is 7 and who is 9";
+
+	int pos  = identifier.find_last_of(digits);
+
+	
 	return 0;
 }
