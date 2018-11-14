277112f7ab6b3e7afa9d4da73569d96e0869632b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 07:17:06 +0600

    c++ string

--- 7. C++ String Class/1. string/20.rfind.cpp
+++ 7. C++ String Class/1. string/20.rfind.cpp
@@ -20,5 +20,15 @@ int main(int argc, char const *argv[])
 
 	str = "a pin in a haystack. pin is really small, so it's really hard to find a pin in haystack";
 
+	if (pos >= 0)
+	{
+		cout << " found pin in position " << pos << endl;
+	}
+	else
+	{
+		cout << " no pin " << pos << endl;
+	}
+
+
 	return 0;
 }
