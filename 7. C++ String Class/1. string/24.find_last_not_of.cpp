015cbf802ef0e83c94b99c8264171d3e11fe689c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 08:58:37 +0600

    c++ string

--- 7. C++ String Class/1. string/24.find_last_not_of.cpp
+++ 7. C++ String Class/1. string/24.find_last_not_of.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	string digits = "0123456789";
+
+	string identifier = "who is 7 and who is 9";
+
+	int pos = identifier.find_last_not_of(digits);
+
+	if (pos >= 0)
+	{
+		cout << " last occurence of identifier characters at " << pos << endl;
+	}
+
 	return 0;
 }
