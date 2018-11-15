ac63953f72f0d673be68a42bfc4f487f0754f22c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-16 03:59:37 +0600

    c++ string

--- 7. C++ String Class/1. string/2.without_white_spaces.cpp
+++ 7. C++ String Class/1. string/2.without_white_spaces.cpp
@@ -12,3 +12,18 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	Basic input stream cin can be used for c++ string input
+
+	but, cin only takes those string that does not include white spaces
+
+	for examle, we will give following string as input
+
+	Football is a very popular game
+
+	we will get,
+	Football
+
+	the characters after whitespace will be ignored 
+*/
