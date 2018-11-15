eafb113bef8caf0d5d6afaa94721e53faea58190
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-16 04:09:35 +0600

    c++ string

--- 7. C++ String Class/1. string/7.Assign_2.cpp
+++ 7. C++ String Class/1. string/7.Assign_2.cpp
@@ -15,3 +15,23 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+
+/*
+
+	for copying c++ string assign() function has another flavour
+
+	with assign() function we can specify a range in which characters we want to copy in the copied string
+
+	basic syntax :
+
+	string text, cpText ;
+	cpText.assign(text, start_index, end_index);
+
+	here,
+
+	start_index = starting index of the range in the copied string ( text )
+
+	end_index = ending index of the range in the copied string ( text ) 
+
+*/
