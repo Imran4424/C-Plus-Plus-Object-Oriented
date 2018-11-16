3ef6b789ba189fa1e376aa5a4fa3390406a21bb0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-16 11:11:37 +0600

    c++ string

--- 7. C++ String Class/1. string/14.Insert_2.cpp
+++ 7. C++ String Class/1. string/14.Insert_2.cpp
@@ -24,5 +24,12 @@ int main(int argc, char const *argv[])
 	with this version of insert() function, we can add a substring of the given string in the specific of the
 	existing string
 
-	
+	here, first argument is the starting index where we want add the string
+	      first argument can be either plain string or string class object
+
+	      second argument is the string which we want to add
+
+	      third argument is the start_index of the given string from where we want to add
+
+	      fourth argument is the end_index of the given string till where we want to add
 */
