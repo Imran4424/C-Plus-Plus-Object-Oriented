3faf9c9eeb4e09d7d9757aef02a202e8f312603b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-17 14:51:42 +0600

    c++ string

--- 7. C++ String Class/1. string/26.substr.cpp
+++ 7. C++ String Class/1. string/26.substr.cpp
@@ -12,5 +12,10 @@ int main(int argc, char const *argv[])
 	string text = "I love to play games";
 
 	cout << "Full string: " << text << endl;
+
+	string sub_string = text.substr(2,4);
+
+	cout << "Substring: " << sub_string << endl;
+	
 	return 0;
 }
