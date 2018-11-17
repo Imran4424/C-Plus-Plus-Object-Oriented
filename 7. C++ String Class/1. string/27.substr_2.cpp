aaef07e1258fbd15d268d2208d9206ec50dfd4d3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-17 15:07:44 +0600

    c++ string

--- 7. C++ String Class/1. string/27.substr_2.cpp
+++ 7. C++ String Class/1. string/27.substr_2.cpp
@@ -1,3 +1,22 @@
 /*
 	write a cpp program to demonstrate c++ string substr(int start_index) function
 */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	string text = "I love to play games";
+
+	cout << "Full string: " << text << endl;
+
+	string sub_string = text.substr(2,4);
+
+	cout << "Substring: " << sub_string << endl;
+
+	return 0;
+}
