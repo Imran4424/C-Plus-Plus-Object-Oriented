7c1319f52202016422bb2e26ffabb0fe2078d5ce
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-17 15:13:21 +0600

    c++ string

--- 7. C++ String Class/1. string/27.substr_2.cpp
+++ 7. C++ String Class/1. string/27.substr_2.cpp
@@ -22,3 +22,12 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	with this version of substr() we can extract substrings from a given string from a specific position to the 
+	string end
+
+	here, only argument start_index is to determine from where we are going to extract substring
+
+	from start_index position till the end of the string we are going to extract substring from the given string
+*/
