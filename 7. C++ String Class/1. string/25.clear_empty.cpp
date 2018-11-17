12b99cd1d6eca4f0f3c91321e271c4a0e26e5c0a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-17 08:38:26 +0600

    c++ string

--- 7. C++ String Class/1. string/25.clear_empty.cpp
+++ 7. C++ String Class/1. string/25.clear_empty.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 {
 	string text = "I am a Programmer";
 
-	cout << text << endl;
+	cout<< "Current string: " << text << endl;
 
 	cout << "Is the string empty? " ;
 
@@ -34,6 +34,6 @@ int main(int argc, char const *argv[])
 	{
 		cout << "No" << endl;
 	}
-	
+
 	return 0;
 }
