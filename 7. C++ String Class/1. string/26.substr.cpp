6bbe1e51233d7349f343c5251f4caf5d388dfb89
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-17 15:06:35 +0600

    c++ string

--- 7. C++ String Class/1. string/26.substr.cpp
+++ 7. C++ String Class/1. string/26.substr.cpp
@@ -1,5 +1,5 @@
 /*
-	write a cpp program to demonstrate c++ string substr(int start_index, size) function
+	write a cpp program to demonstrate c++ string substr(int start_index, int size) function
 */
 
 #include <iostream>
@@ -21,5 +21,11 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	with this version of substr() we can extract substring from a string 
+	with this version of substr() we can extract substring from a string from any position or range
+
+	here, first argument start_index is the starting position from where we are going to extract substring
+
+	      second argument size is going to determine from start till where we are going to extract substring.
+	      in other words size will determine the the ending of the substring.
+	      we can also call this size substring size.
 */
