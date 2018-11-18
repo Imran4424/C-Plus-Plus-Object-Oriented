3656ad513aa7d44f058dbad7af0547c596f4ed38
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 23:28:39 +0600

    c++ files

---
+++ 9. Files/2. Read_Write_2/3.Read_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	string fileName = "text.txt";
+
+	fstream readFile;
+
+	readFile.open(fileName, ios::in);
+
+	if (readFile.is_open())
+	{
+		string line;
+
+		while(readFile)
+		{
+			getline(readFile, line);
+
+			cout << line << endl;
+		}
+
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file:" << fileName << endl;
+	}
+
+	return 0;
+}
