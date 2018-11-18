70cbdb8433f3a2a04e187071e6fce5221b04202c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 08:42:21 +0600

    c++ string

---
+++ 9. Files/4. Read_Write_4/1.Read_Write.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	string fileName = "text.txt";
+
+	fstream file;
+
+	file.open(fileName, ios::out);
+
+	if (file.is_open())
+	{
+		file << "I am a Programmer" << endl;
+		file << "I love to play" << endl;
+		file << "I love to work game and software development" << endl;
+		file << "My id is: " << 1510176113 << endl;
+
+		file.close();
+	}
+	else
+	{
+		cout << "can not write the file: " << fileName << endl;
+	}
+
+	file.open(fileName, ios::in);
+
+	if (file.is_open())
+	{
+		string line;
+
+		while(file)
+		{
+			getline(file, line);
+
+			cout << line << endl;
+		}
+
+		file.close();
+	}
+	else
+	{
+		cout << "can not read file: " << endl;
+	}
+	return 0;
+}
