d625685c15ac86c63f55cf461784c98992c18518
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 07:01:37 +0600

    c++ string

--- 9. Files/1. Read_Write_1/4.Read_3.cpp
+++ 9. Files/1. Read_Write_1/4.Read_3.cpp
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
 	{
 		string line;
 
-		while(!readFile.eof())
+		while(readFile)
 		{
 			getline(readFile, line);
 
---
+++ 9. Files/2. Read_Write_2/1.Write.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	ofstream writeFile;
+
+	string fileName = "text.txt";
+
+	writeFile.open(fileName);
+
+	if (writeFile.is_open())
+	{
+		writeFile << "I am a Programmer" << endl;
+		writeFile << "I love to play" << endl;
+		writeFile << "I love to work game and software development" << endl;
+		writeFile << "My id is: " << 1510176113 << endl;
+
+		writeFile.close();
+	}
+	else
+	{
+		cout << "Can not create file:" << fileName << endl;
+	}
+
+	return 0;
+}
