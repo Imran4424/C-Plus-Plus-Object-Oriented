3e4b825528e31de3c3a4512cbfefdf0f3ea76500
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 17:26:05 +0600

    c++ string

--- 9. Files/5. Parsing Text Files/1.Parsing_Text_File.cpp
+++ 9. Files/5. Parsing Text Files/1.Parsing_Text_File.cpp
@@ -5,6 +5,15 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	string fileName = "text.txt";
+
+	ifstream readFile;
+
+	readFile.open(fileName);
+
+	if (readFile.is_open())
+	{
+		/* code */
+	}
 	return 0;
 }
