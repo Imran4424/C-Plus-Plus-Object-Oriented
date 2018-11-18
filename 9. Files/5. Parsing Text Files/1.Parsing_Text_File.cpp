1281c6cd6c0992d07e21197dec47f38844c6c519
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 17:57:00 +0600

    c++ string

--- 9. Files/5. Parsing Text Files/1.Parsing_Text_File.cpp
+++ 9. Files/5. Parsing Text Files/1.Parsing_Text_File.cpp
@@ -44,3 +44,47 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	for file parsing, c++ gives very little support but we can do it by c++ fstream
+
+	In this code we are parsing text file to int data type line by line
+
+	for this, we are reading text till ':' in one line using the following statement
+
+		getline(readFile, line, ':');
+
+
+	text to int parsing is happening using the following statement
+
+		int population;
+		readFile >> population;
+
+		
+	but there is problem, when we again reading the file after one text to int parsing
+
+	there is a newline nonprintable character calls white space which is treated as another line
+
+	because, our getline ending character is ':' 
+
+		getline(readFile, line, ':');
+
+	so we have to skip white spaces, we can do it in c++11 by simply using the following statement
+		
+		readFile >> ws;
+
+
+	in the while loop, we are extracting content two times from the file but we are checking if the file is
+	                   is empty or not for once in the while loop condition
+
+	                   that can give us some garbage output too
+
+	for fixing that problem we can simply use the following statement after parsing text to int
+
+		if (!readFile) 
+		{
+			break;
+		}
+
+	it will break the while loop if readFile is empty
+*/
