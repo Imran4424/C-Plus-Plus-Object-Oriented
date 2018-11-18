995acc84ac941e846b7bf322810c716f58a77eda
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-18 06:47:46 +0600

    c++ string

---
+++ 9. Files/1. Open_Close/4.Read_3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	string fileName = "text.txt";
+
+	ifstream readFile;
+
+	readFile.open(fileName);
+
+	if (readFile.is_open())
+	{
+		string line;
+
+		while(!readFile.eof())
+		{
+			getline(readFile, line);
+
+			cout << line << endl;			
+		}
+
+
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file" << endl;
+	}
+
+	return 0;
+}
