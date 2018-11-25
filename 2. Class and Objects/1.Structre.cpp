209fe213c360b452b98b8f7bbf67fa00822d3826
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-25 23:14:54 +0600

    class and objects

--- 2. Class and Objects/1.Structre.cpp
+++ 2. Class and Objects/1.Structre.cpp
@@ -34,5 +34,24 @@ int main(int argc, char const *argv[])
 
 	for learning classes first we need to understand the basic operations of structres
 
-	although structure is completely different from 
+	although classes are different from structure, the low level basic concept of classes are same as
+	structures
+
+
+	because classes are build on the basic concept of structures and evolve into a new programming paradism
+	called object oriented programming.
+
+
+	now, let's talk about struct
+
+	structs are mainly a package of variables and functions, it is also called as user defined data types
+
+	in a struct code block we can declare multiple variables and functions in a single code block as for the
+	requirements and access them using struct type variable
+
+	for accessing struct member variables we use dot(.) operators, sometimes which is called member access
+	operator
+
+
+	see the above code for better understanding
 */
