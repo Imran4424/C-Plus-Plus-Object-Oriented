039fd73c421b7a6eeb4e0a96d055e55514f8355b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-25 08:42:13 +0600

    files

--- 9. Files/1. Read_Write_1/3.Read_2.cpp
+++ 9. Files/1. Read_Write_1/3.Read_2.cpp
@@ -37,3 +37,24 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+
+/*
+	As we said one problem with getline is it stops reading from file whenever it gets endline character
+
+	we can solve this problem using a loop 
+
+		string line;
+
+		getline(obj, line);
+	
+	one problem with getline, by default it reads from file untill it gets a new line character and once it
+	gets new line character it stops reading
+
+	and you can check if the file is open or not using is_open() function
+
+	is_open() function returns true if the file is open, is_open() function returns false if the file stream
+	is fails to open the file.
+
+	for good practice we need to always close the file after ending all operations on the file
+*/
