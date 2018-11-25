e1fc4248fe62023c459add6a491134788acbef79
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-25 08:51:06 +0600

    files

--- 9. Files/1. Read_Write_1/3.Read_2.cpp
+++ 9. Files/1. Read_Write_1/3.Read_2.cpp
@@ -44,11 +44,23 @@ int main(int argc, char const *argv[])
 
 	we can solve this problem using a loop, by using loop we can read the file again from where that ends
 	reading when it finds the endline character cause file pointer don't change after having a new line
-	character just it sends the pointer to new line. and keep repeati
+	character just it sends the pointer to new line. and keep repeating this until the file is finished 
+	we can read the whole file.
 
-		string line;
+	for example,
 
-		getline(obj, line);
+
+		if (readFile.is_open())
+		{
+			string line;
+
+			while(!readFile.eof())
+			{
+				getline(readFile, line);
+
+				cout << line << endl;			
+			}
+		}
 	
 	one problem with getline, by default it reads from file untill it gets a new line character and once it
 	gets new line character it stops reading
