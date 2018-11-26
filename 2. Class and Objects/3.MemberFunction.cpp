119581206ad289b03975077261cde49d4a3341f3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-26 08:59:40 +0600

    class and objects

---
+++ 2. Class and Objects/3.MemberFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+public:
+	int legth, width;
+
+	void Assign(int l, int w)
+	{
+		
+	}
+	
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
