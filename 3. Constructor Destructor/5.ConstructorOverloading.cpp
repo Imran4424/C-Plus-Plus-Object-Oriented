893acfb5738669721ee0c997c28af6978e715239
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-28 17:17:31 +0600

    constructor destructor

---
+++ 3. Constructor Destructor/5.ConstructorOverloading.cpp
@@ -0,0 +1,47 @@
+/*
+	write a program to demonstrate constructor overloading
+*/
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+	private: int length, width;
+	public: Box()
+	{
+		length = 0;
+		width = 0;
+	}
+
+	public: void Assign(int l, int w)
+	{
+		length = l;
+		width = w;
+	}
+	
+	public: int Area()
+	{
+		return length*width;
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	Box red;
+
+	cout << "Area is: " << red.Area() << endl;
+
+	return 0;
+}
+
+/*
+	default constructor is a special type user define constructor which has no parameters
+
+	in the default constructor anyone can use default values to initialize object variables
+
+	in this code 0 as default values for Box object
+
+	but remember this default values can be anything means any valid value
+*/
