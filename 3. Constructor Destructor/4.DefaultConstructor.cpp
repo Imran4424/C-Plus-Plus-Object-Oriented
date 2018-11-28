e6471c9f70f0eb1b8369d044c94dcb9bd40b3cf4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-28 09:38:00 +0600

    constructor destructor

--- 3. Constructor Destructor/4.DefaultConstructor.cpp
+++ 3. Constructor Destructor/4.DefaultConstructor.cpp
@@ -1,3 +1,25 @@
 /*
-	write a program to demonstrate 
+	write a program to demonstrate default constructor in c++ classes
 */
+
+class Box
+{
+	private: int length, width;
+	public: Box()
+	{
+		length = 0;
+		width = 0;
+	}
+
+	public: void Assign(int l, int w)
+	{
+		length = l;
+		width = w;
+	}
+	
+	public: int Area()
+	{
+		return length*width;
+	}
+};
+
