472ab18f5ee6cedea719d2b0b424cc3131e9696b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 06:26:04 +0600

    constructor destructor

--- 3. Constructor Destructor/7.ArrayInitialization.cpp
+++ 3. Constructor Destructor/7.ArrayInitialization.cpp
@@ -1,3 +1,17 @@
 /*
-	write a program to demonstrate 
+	write a program to demonstrate Array Initialization
 */
+
+#include <iostream>
+using namespace std;
+
+
+class Number
+{
+	private: const int m_array[5];
+
+	public: Number(): m_array {} // initialize zero to the member array
+	{
+		// for assignment or other purposes we can use this block
+	}
+};
