3be738cdafa73dc1a3edad0749544daae27473da
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 09:11:47 +0600

    constructor destructor

---
+++ 3. Constructor Destructor/12.NonStaticMemberInitialization_2.cpp
@@ -0,0 +1,48 @@
+/*
+	write a program to demonstrate non-static member initialization also called as in-class member 
+	initialization
+*/
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+	private: int length = 0, width = 0;
+
+	public: Box(int l) 
+	{
+		length = l;
+	}
+
+	public: Box(int l, int w)
+	{
+		length = l;
+		width = w;
+	}
+	
+	public: int Area()
+	{
+		return length*width;
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	Box red; // now this code won't give error
+
+	cout << "Area is: " << red.Area() << endl;
+
+	Box blue(5);
+
+	cout << "Area is: " << blue.Area() << endl;
+
+
+	Box green(8, 13);
+
+	cout << "Area is: " << green.Area() << endl;
+
+	return 0;
+}
+
