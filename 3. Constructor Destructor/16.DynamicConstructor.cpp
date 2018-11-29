0b504b17ddd29c295b7566e9f3af17d18a004245
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 19:16:09 +0600

    constructor destructor

--- 3. Constructor Destructor/16.DynamicConstructor.cpp
+++ 3. Constructor Destructor/16.DynamicConstructor.cpp
@@ -1,3 +1,14 @@
 /*
 	write a program to demonstrate dynamic constructor in c++
 */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class DynamicConstructor
+{
+	private: char *name;
+	private: int length;
+	
+};
