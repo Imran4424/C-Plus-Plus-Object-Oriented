75df38e3da29313229faf6ed6d8909f7e4959685
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 07:03:51 +0600

    constructor destructor

--- 3. Constructor Destructor/9.MemberClassConstructor.cpp
+++ 3. Constructor Destructor/9.MemberClassConstructor.cpp
@@ -2,3 +2,36 @@
 	write a program to demonstrate member class constructor
 */
 
+#include <iostream>
+using namespace std;
+
+
+class A
+{
+
+	public: A()
+	{
+		cout << "Calling from constructor A" << endl;
+	}
+	public: ~A()
+	{
+		cout << "Calling from destructor A" << endl;
+	}
+	
+};
+
+
+class B
+{
+	private: A obj;
+
+	public: B()
+	{
+		cout << "Calling from constructor B" << endl;
+	}
+	public: ~B()
+	{
+		cout << "Calling from destructor B" << endl;
+	}
+	
+};
