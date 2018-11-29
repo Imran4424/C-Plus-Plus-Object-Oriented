334cf9fc263ad43c932ea3172b619b6d9b666288
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 19:31:01 +0600

    constructor destructor

--- 3. Constructor Destructor/16.DynamicConstructor.cpp
+++ 3. Constructor Destructor/16.DynamicConstructor.cpp
@@ -10,5 +10,18 @@ class DynamicConstructor
 {
 	private: char *name;
 	private: int length;
+
+	public: DynamicConstructor()
+	{
+		length = 0;
+		name = new char(length+1);
+	}
 	
+	public: DynamicConstructor(char *line)
+	{
+		length = strlen(line);
+		name = new char(length+1);
+
+		strcpy(name, line);
+	}
 };
