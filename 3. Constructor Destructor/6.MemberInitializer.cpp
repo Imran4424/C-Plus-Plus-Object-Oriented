3dca10a40cfdbb8c7ac45aca8a9c5eb90a9fcfc3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 08:55:28 +0600

    constructor destructor

--- 3. Constructor Destructor/6.MemberInitializer.cpp
+++ 3. Constructor Destructor/6.MemberInitializer.cpp
@@ -61,4 +61,20 @@ int main(int argc, char const *argv[])
 	there is solution for that named "member initializer lists"
 
 	see the code for coding example
+
+
+	One remainder (next text block from learncpp)
+
+	Perhaps surprisingly, variables in the initializer list are not initialized in the order that they are 
+	specified in the initializer list. Instead, they are initialized in the order in which they are declared
+	in the class.
+
+	For best results, the following recommendations should be observed:
+	1) Don’t initialize member variables in such a way that they are dependent upon other member variables 
+	   being initialized first (in other words, ensure your member variables will properly initialize even 
+	   if the initialization ordering is different).
+	2) Initialize variables in the initializer list in the same order in which they are declared in your 
+	   class. This isn’t strictly required so long as the prior recommendation has been followed, but your 
+	   compiler may give you a warning if you don’t do so and you have all warnings turned on.
+
 */
