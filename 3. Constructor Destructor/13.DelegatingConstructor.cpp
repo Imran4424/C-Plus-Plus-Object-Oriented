3c28f5f32d2416069f7f7b5b00403f849c7391b7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-29 10:11:01 +0600

    constructor destructor

--- 3. Constructor Destructor/13.DelegatingConstructor.cpp
+++ 3. Constructor Destructor/13.DelegatingConstructor.cpp
@@ -2,3 +2,15 @@
 	write a program to demonstrate how to delegate constructor means calling constructor from constructor
 	(this work from c++ 11)
 */
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
