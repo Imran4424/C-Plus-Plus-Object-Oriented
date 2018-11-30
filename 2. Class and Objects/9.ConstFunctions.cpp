1558aa444d7638114ef88b9c2608de91e22d94f1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-30 15:51:09 +0600

    constructor destructor

--- 2. Class and Objects/9.ConstFunctions.cpp
+++ 2. Class and Objects/9.ConstFunctions.cpp
@@ -1,5 +1,5 @@
 /*
-	write a program to demonstrate const objects
+	write a program to demonstrate const functions
 */
 
 #include <iostream>
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[])
 
 	const Box red(length, width);
 
-	cout << "Area of the Box is: " << red.Area() << endl; // this line of code will give you error
+	cout << "Area of the Box is: " << red.Area() << endl; // now this line of code won't give you error
 
 	return 0;
 }
