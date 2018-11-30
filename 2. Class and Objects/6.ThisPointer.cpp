059a00e4d43a23d24a0ed088799f55ab9ae63a23
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-30 15:15:59 +0600

    constructor destructor

--- 2. Class and Objects/6.ThisPointer.cpp
+++ 2. Class and Objects/6.ThisPointer.cpp
@@ -24,7 +24,26 @@ class Box
 
 int main(int argc, char const *argv[])
 {
-	
-	
+	cout << "input length and width of a box" << endl;
+
+	int length,width;
+	cin >> length >> width;
+
+	Box red;
+
+	red.Assign(length, width);
+
+	cout << "Area of the Box is: " << red.Area() << endl;
+
 	return 0;
 }
+
+/*
+	there are two types of this pointer in c++
+
+	implicit this pointer
+	explicit this pointer
+
+	implicit this pointer - 
+
+*/
