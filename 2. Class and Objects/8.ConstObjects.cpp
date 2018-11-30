7470314787a5a16969bbe6d2d6984ccdcb5897c9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-30 15:46:28 +0600

    constructor destructor

--- 2. Class and Objects/8.ConstObjects.cpp
+++ 2. Class and Objects/8.ConstObjects.cpp
@@ -2,3 +2,38 @@
 	write a program to demonstrate const objects
 */
 
+#include <iostream>
+using namespace std;
+
+class Box
+{
+	private: int length, width;
+
+	public: Box(int length, int width)
+	{
+		this -> length = length;
+		this -> width = width;
+	}
+	
+	public: int Area()
+	{
+		return length * width;
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	cout << "input length and width of a box" << endl;
+
+	int length,width;
+	cin >> length >> width;
+
+	Box red;
+
+	red.Assign(length, width);
+
+	cout << "Area of the Box is: " << red.Area() << endl;
+
+	return 0;
+}
