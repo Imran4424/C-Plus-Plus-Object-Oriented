9cfcfbc1ef4f45b6db593ea39ee823bed8d39193
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-01 10:41:16 +0600

    constructor destructor

--- 2. Class and Objects/11.StaticMemberVariables_2.cpp
+++ 2. Class and Objects/11.StaticMemberVariables_2.cpp
@@ -28,15 +28,14 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	as we know, static variable works like global variable, it creates it's existence when the code runs
-	destroys itself after the program finished
+	in real scenario, static variable don't belong to any object
+
+	it belong to the class
+
+	we can use static variable using class name with scope resolution, for example,
+
+		Number::value
 
 
-	now, if we can make a class member variable static using static keyword, then we need to define the
-	static varibale reference
-
-	static member variables are shared between all objects of a class, so if you change the static variable
-	using one object
-
-	the change will effect other object too
+	one thing you can't access the static variable if it is static
 */
