446ed0a41cd4084e106fbdcdd63d3ccb707f2539
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-01 09:28:09 +0600

    constructor destructor

--- 2. Class and Objects/10.StaticMemberVariables.cpp
+++ 2. Class and Objects/10.StaticMemberVariables.cpp
@@ -1,3 +1,12 @@
 /*
 	write a program to demonstrate static member variables in c++ class
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
