79380713e506dd57de359f6765f3bc7c62af2503
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-01 16:15:29 +0600

    constructor destructor

--- 2. Class and Objects/13.StaticMemberFunction.cpp
+++ 2. Class and Objects/13.StaticMemberFunction.cpp
@@ -51,9 +51,23 @@ int main(int argc, char const *argv[])
 	static member function has no this(*) pointer because static member functions are not attach to
 	any class objects
 
+	static member functions can directly access other static members (variables or functions), 
+	but not non-static members
 
 	when a class's all member became static then the class is called pure static classes
 
 	in a pure static class we don't have to instantiate any object cause we can access all members
 	directy through class
+
+
+	there is some downside of pure static class
+
+	Firstly, because all static members are instantiated only once, there is no way to have multiple 
+	copies of a pure static class. And if you need two independent copies of a class member, you
+	can't have it
+
+	Secondly, Because all of the members belong to the class and class declarations usually have global 
+	scope, a pure static class is essentially the equivalent of declaring functions and global 
+	variables in a globally accessible namespace, with all the requisite downsides that global 
+	variables have 
 */
