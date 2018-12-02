3a68ed8f72e3bc8c23efdcb289bf22476d5fb9fd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-02 06:54:39 +0600

    constructor destructor

--- 2. Class and Objects/14.StaticConstructors.cpp
+++ 2. Class and Objects/14.StaticConstructors.cpp
@@ -1,3 +1,21 @@
 /*
 	wrtie a program to demonstrate static constructor in c++
 */
+
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+	private: int length, width, height;
+
+	public: static Box()
+	{
+		length = 0;
+		width = 0;
+		height = 0;
+
+		cout << "Static constructor" << endl;
+	}
+};
