31d1a55886dde46cb6d44cd1b24b477173036179
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-02 07:16:01 +0600

    constructor destructor

---
+++ 2. Class and Objects/15.FriendFunction.cpp
@@ -0,0 +1,32 @@
+class Accumulator
+{
+
+    private: int value;
+
+    public: Accumulator
+    {
+        value = 0;
+    }
+
+    public: void Add(int num)
+    {
+        value = value + num;
+    }
+    friend void reset(Accumulator &accumulator);
+};
+ 
+// reset() is now a friend of the Accumulator class
+void reset(Accumulator &accumulator)
+{
+    // And can access the private data of Accumulator objects
+    accumulator.m_value = 0;
+}
+ 
+int main()
+{
+    Accumulator acc;
+    acc.add(5); // add 5 to the accumulator
+    reset(acc); // reset the accumulator to 0
+ 
+    return 0;
+}
