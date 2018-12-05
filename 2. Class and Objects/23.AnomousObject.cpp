9286fcade9f6a14e9e676320ebce634d85316e3a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-05 06:56:55 +0600

    class object

---
+++ 2. Class and Objects/23.AnomousObject.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int Add(int x, int y)
+{
+	return x+y;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	cout << Add(7, 5) << endl;
+	
+	return 0;
+}
