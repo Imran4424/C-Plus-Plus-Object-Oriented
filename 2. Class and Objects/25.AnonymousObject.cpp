b8962d689235076bef49eadb5b42f1eea2737999
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-06 08:37:35 +0600

    class object

--- 2. Class and Objects/25.AnonymousObject.cpp
+++ 2. Class and Objects/25.AnonymousObject.cpp
@@ -9,9 +9,8 @@ void Print(int value)
 
 int main(int argc, char const *argv[])
 {
-	int addition = 7 + 10;
-
-	Print(addition);
+	
+	Print(12 + 9);  // an anonymous object is created to hold the vlue of 12+9 and then pass it to the Print
 	
 	return 0;
 }
