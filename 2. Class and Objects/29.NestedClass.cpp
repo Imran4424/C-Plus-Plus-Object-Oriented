1a384cf786a9c16d2fba3dfaad32f9df428ea6f4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-07 04:09:23 +0600

    class object

--- 2. Class and Objects/29.NestedClass.cpp
+++ 2. Class and Objects/29.NestedClass.cpp
@@ -1,1 +1,18 @@
- 
+ #include <iostream>
+using namespace std;
+
+
+class Enclosing
+{
+	private: int x = 10, y = 12;
+
+	class Nested
+	{
+		private: int a = 7, b = 11;
+
+		public: void Show()
+		{
+			
+		}
+	};
+};
