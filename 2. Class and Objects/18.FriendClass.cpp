76f5ace5ed3372f16ff2c5ed48247a4809a804fb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-06 06:15:27 +0600

    class object

--- 2. Class and Objects/18.FriendClass.cpp
+++ 2. Class and Objects/18.FriendClass.cpp
@@ -1,3 +1,34 @@
 /*
 	write a program to demonstrate friend class in c++
 */
+
+#include <iostream>
+using namespace std;
+
+
+class Box
+{
+	private: int length, width, height;
+
+	public: Box()
+	{
+		length = 0;
+		width = 0;
+		height = 0;
+	}
+
+	public: Box(int l)
+	{
+		Box()
+
+		length = l;
+	}
+
+	public: Box(int l, int w)
+	{
+		Box();
+
+		length = l;
+		width = w;
+	}	
+};
