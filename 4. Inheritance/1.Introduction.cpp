8069526ab2fddc35b16d43c2c0730903c6703c6d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-07 10:02:52 +0600

    class object

--- 4. Inheritance/1.Introduction.cpp
+++ 4. Inheritance/1.Introduction.cpp
@@ -1,3 +1,7 @@
+/*
+	writa a program to demonstrate inheritance in c++
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -27,4 +31,45 @@ class Student: public Person
 {
 	public: int studentID;
 	public: string institution;
+
+	public: Student()
+	{
+		name = "";
+		age = 0;
+		studentID = 0;
+		institution = "";
+	}
+
+	public: Student(string name, int age, int studentID, string institution)
+	{
+		this -> name = name;
+		this -> age = age;
+		this -> studentID = studentID;
+		this -> institution = institution;
+	}
+
+	public: void Show()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Student Id: " << studentID << endl;
+		cout << "Institution: " << institution << endl;  
+	}
 };
+
+int main(int argc, char const *argv[])
+{
+
+	string name = "Shah Md. Imran Hossain";
+	int age = 15;
+	int studentID = 48;
+	string institution = "Ranpur Zilla School, Ranpur";
+
+	
+
+	Student rzsian(name, age, studentID, institution);
+
+	rzsian.Show();
+
+	return 0;
+}
