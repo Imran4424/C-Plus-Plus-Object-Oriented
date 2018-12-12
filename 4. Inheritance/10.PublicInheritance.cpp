366c3b01cce6805ab2ef0cccfe74c0387a7bb2ee
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-12 09:24:17 +0600

    Inheritance

--- 4. Inheritance/10.PublicInheritance.cpp
+++ 4. Inheritance/10.PublicInheritance.cpp
@@ -1,3 +1,19 @@
 /*
 	write a program to demonstrate public inheritance in big picture
 */
+
+class Base
+{
+	private: int id;
+	public: int price;
+	protected: int totalCost;
+
+	public: Base()
+	{
+		id = 0;
+		price = 0;
+		totalCost = 0;
+	}
+
+	
+};
