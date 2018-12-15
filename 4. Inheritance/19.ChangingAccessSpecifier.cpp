40d7b5aa602312d5791e2c58cdadc5b37f4956a0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-15 16:15:29 +0600

    Inheritance

--- 4. Inheritance/19.ChangingAccessSpecifier.cpp
+++ 4. Inheritance/19.ChangingAccessSpecifier.cpp
@@ -77,43 +77,5 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	so far we have been, two types of access specifier - public, private
-	in this code we will see about protected access specifier
-
-	public - public class members can be accessed by anyone through an object
-	private - private class members can be accessed only by same class members
-
-	protected - protected class members can be accessed by same class members as well as the derived class
-	            members
-
-	otherwise it will generate error
-
-
-
-	now let's talk about Inheritance type
-
-	a class can be inherited in 3 types - public , private , protected
-
-
-
-
-	In this code we will only talk about protected inheritance
-
-	when we inherit a class in protected mode then all class members changes their access specifier in 
-	derived class. And private class members not even exist in derived class.
-
-	actually we can't inherited private members from base classes, that's why we can't access private base
-	members from derived class, but we can always use a public member funtion in base class to access the
-	private base member
-
-
-	Base class - Derived class
-
-	private    - not inherited / Inaccessible
-	public     - protected
-	protected  - protected
-
 	
-	that means if you inherited something in protected mode from base class then private member variables 
-	doesn't inherited from base class, and public and protected members became protected in derived class
 */
