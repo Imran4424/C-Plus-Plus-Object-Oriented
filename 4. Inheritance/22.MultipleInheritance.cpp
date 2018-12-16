c02ba36cdd0227083a35036d7dfb6204201218e3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-16 13:08:30 +0600

    inheritance

--- 4. Inheritance/22.MultipleInheritance.cpp
+++ 4. Inheritance/22.MultipleInheritance.cpp
@@ -7,9 +7,71 @@
 #include <string>
 using namespace std;
 
+class Person // Base
+{
+	public: string name;
+	public: int age;
+
+	public: Person()
+	{
+		name = "";
+		age = 0;
+	}
+
+	public: Person(string name, int age)
+	{
+		this -> name = name;
+		this -> age = age;
+	}
+
+};
+
+class Employee
+{
+	public: int wage;
+
+	public: Employee()
+	{
+		wage = 0;
+	}
+
+	public: Employee(int wage)
+	{
+		this -> wage = wage;
+	}
+};
+
+
+class Teacher: public Person, public Employee
+{
+	public: string instition;
+
+	public: Teacher()
+	{
+		instition = "";
+	}
+
+	public: Teacher(string name, int age, int wage, string instition): Person(name, age), Employee(wage)
+	{
+		this -> instition = instition;
+	}
+
+
+	public: void Show()
+	{
+		cout<< "Name: " << name << endl;
+		cout<< "Age: " << age << endl;
+		cout<< "Wage: " << wage << "$" << endl;
+		cout<< "Instition: " << instition << endl;
+	}
+};
+
 
 int main(int argc, char const *argv[])
 {
-	
+	Teacher green("Sakib", 30, 200000, "MIT");
+
+	green.Show();
+
 	return 0;
 }
