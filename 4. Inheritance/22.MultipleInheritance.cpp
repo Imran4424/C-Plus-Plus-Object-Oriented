62ca0a084e1b0bc8cc06efdff9b428b961026c46
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-16 12:27:09 +0600

    inheritance

---
+++ 4. Inheritance/22.MultipleInheritance.cpp
@@ -0,0 +1,15 @@
+/*
+	write a program to demonstrate multiple inheritance in c++
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
