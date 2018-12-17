bf9f7498b78a27fc2d185c6f1a6f4c1ebe04cef0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-17 16:36:21 +0600

    Virtual function

--- 5. Virtual Functions/1.Pointers.cpp
+++ 5. Virtual Functions/1.Pointers.cpp
@@ -9,6 +9,51 @@ using namespace std;
 
 class Person
 {
-	public: int age;
 	public: string profession;
+
+	public: Person()
+	{
+		profession = "";
+	}
+
+	public: Person(int age, string job)
+	{
+		this -> profession = profession;
+	}
+
+
+	public: string GetJob()
+	{
+		return "none";
+	}
 };
+
+class Engineer: public Person
+{
+	public: Engineer(): Person()
+	{
+
+	}
+
+	public: Engineer(string profession): Person(profession)
+	{
+
+	}
+
+	public: string GetJob()
+	{
+		return "Develop App";
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Engineer eng("Computer");
+
+	cout << eng.GetJob() << endl;
+	Person *ptr = & eng;
+
+	cout << ptr -> GetJob() << endl;
+
+	return 0;
+}
