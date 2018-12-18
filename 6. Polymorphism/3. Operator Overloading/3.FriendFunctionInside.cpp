711a5ffbec065dd7a6127b0bf2ca236db2d2c080
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-18 19:50:04 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/3.FriendFunctionInside.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class ChocolateBox
+{
+	private: int countChocolate;
+
+	public: ChocolateBox()
+	{
+		countChocolate = 0;
+	}
+
+	public: ChocolateBox(int count)
+	{
+		countChocolate = count;
+	}
+
+	public: int GetChocolateCount()
+	{
+		return countChocolate;
+	}
+
+	friend ChocolateBox operator+(const ChocolateBox &kitkat, const ChocolateBox &fiveStar)
+	{
+		return ChocolateBox(kitkat.countChocolate + fiveStar.countChocolate);
+	}
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+	ChocolateBox kitkat(15);
+	ChocolateBox fiveStar(30);
+
+	ChocolateBox total = kitkat + fiveStar; 
+
+	cout << "I have " << total.GetChocolateCount() << " chocolates in my box." << endl;
+
+	
+	return 0;
+}
