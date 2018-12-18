92eea907467b12f653c2eafce5935e1bbc5a5bfe
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-18 19:29:34 +0600

    operator overloading

--- 6. Polymorphism/3. Operator Overloading/1.FriendFuction.cpp
+++ 6. Polymorphism/3. Operator Overloading/1.FriendFuction.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class ChocolateBox
+{
+	private: int countChocolate;
 
+	public: ChocolateBox()
+	{
+		countChocolate = 0;
+	}
+
+	public: ChocolateBox(int count)
+	{
+		countChocolate = count
+	}
+
+	public: int GetChocolateCount()
+	{
+		return countChocolate;
+	}
+};
 
 int main(int argc, char const *argv[])
 {
