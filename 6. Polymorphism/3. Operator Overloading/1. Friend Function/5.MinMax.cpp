ec42138a38f27d57103df5e68fa24a52e8f1a48d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-19 10:33:11 +0600

    operator overloading

--- 6. Polymorphism/3. Operator Overloading/1. Friend Function/5.MinMax.cpp
+++ 6. Polymorphism/3. Operator Overloading/1. Friend Function/5.MinMax.cpp
@@ -118,8 +118,14 @@ int main(int argc, char const *argv[])
 }
 
 /*
-	the above equation will evaluate like this
+	the above arithmetic expression will evaluate like this
 
-	((((((kitkat + fiveStar) + cadbury) + 27) + 22) + galaxy) + 3)
-	
+	ChocolateBox total = ((((((kitkat + fiveStar) + cadbury) + 27) + 22) + galaxy) + 3)
+
+
+	here, we don't use any function for value + value (ex: 7 + 12)
+
+	so, following type of arithmetic expression will give error
+
+	ChocolateBox total = 7 + 12 + kitkat + fivestar + 3 + galaxy;
 */
