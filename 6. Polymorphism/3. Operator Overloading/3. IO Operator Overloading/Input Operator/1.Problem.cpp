65f670a23161a21a020da39627c9eae27c5a1a36
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-22 08:10:06 +0600

    input operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/3. IO Operator Overloading/Input Operator/1.Problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Student
+{
+	private: string name;
+	private: int age;
+	private: int id;
+	private: string department;
+	private: string institution;
+
+	public: Student()
+	{
+		name = "";
+		age = 0;
+		id = 0;
+		department = "";
+		institution = "";
+	}
+}
