33616ad64283a878a4aaf9d443d74b38f448806d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 13:46:03 +0600

    unary operators overloading

---
+++ 6. Polymorphism/3. Operator Overloading/5. Unary Operators/2.Negative.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Data
+{
+	private: int x,y;
+	private: bool isPositive;
+
+	public: Data()
+	{
+		x = 0;
+		y = 0;
+		isPositive = true;
+	}
+
+	public: Data(int x, int y, bool isPositive)
+	{
+		this -> x = x;
+		this -> y = y;
+		this -> isPositive = isPositive;
+	}
+
+	public: void Show()
+	{
+		cout << "X = " << x << endl;
+		cout << "Y = " << y << endl;
+		cout << "Is Postivie = " << boolalpha << isPositive << endl << endl;
+	}
+
+	public: Data operator+()
+	{
+		return Data(abs(x), abs(y), true);
+	}
+
+	public: Data operator-()
+	{
+		return Data(-abs(x), -abs(y), false);
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Data red(-5, -8, false);
+
+	red.Show();
+
+	(+red).Show();
+
+	// (red+).Show(); // this statement will give you error
+	              // because unary operators sits left of operands
+	
+
+	(-red).Show();
+	
+	return 0;
+}
