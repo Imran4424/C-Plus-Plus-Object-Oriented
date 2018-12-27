46dacc5e3c55e4cdcbf98215de5af3b6eddd60db
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 07:21:49 +0600

    operator overloading - member function

---
+++ 6. Polymorphism/3. Operator Overloading/4. Member Function/3.Multiplication.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class ChocolateBox
+{
+	private: int countChocolate;
+
+	public: ChocolateBox()
+	{
+		countChocolate = 0;
+	}
+
+	public: ChocolateBox(int count)
+	{
+		countChocolate = count;
+	}
+
+	public: int GetChocolateCount()
+	{
+		return countChocolate;
+	}
+
+	public: ChocolateBox operator*(int value)
+	{
+		return ChocolateBox(countChocolate * value);
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+	ChocolateBox kitkat(15);
+
+	ChocolateBox total = kitkat * 5; 
+
+	cout << "I have " << total.GetChocolateCount() << " chocolates in my box." << endl;
+
+	
+	return 0;
+}
