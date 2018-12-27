4d3237a295f895edc4db4fcfd91e90ba29a2ab8d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 09:34:10 +0600

    operator overloading - member function

--- 6. Polymorphism/3. Operator Overloading/5. Unary Operators/1.Positive.cpp
+++ 6. Polymorphism/3. Operator Overloading/5. Unary Operators/1.Positive.cpp
@@ -4,10 +4,19 @@ using namespace std;
 class Data
 {
 	private: int x,y;
-	private: bool IsPositive;
+	private: bool isPositive;
 
 	Data()
 	{
+		x = 0;
+		y = 0;
+		IsPositive = true;
+	}
 
+	Data(int x, int y, bool isPositive)
+	{
+		this -> x = x;
+		this -> y = y;
+		this -> isPositive = isPositive;
 	}
 };
