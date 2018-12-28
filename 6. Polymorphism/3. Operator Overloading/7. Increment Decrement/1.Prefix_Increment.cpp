e1338362129dcb6d08b5f29953db7f054bf90cab
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-28 15:56:34 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/7. Increment Decrement/1.Prefix_Increment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Chocolate
+{
+	private: int countChocolate;
+
+	public: Chocolate()
+	{
+		countChocolate = 0;
+	}
+
+	public: Chocolate(int count)
+	{
+		countChocolate = count;
+	}
+}
