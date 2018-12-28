0d9432a3fafc95c7da01fc4d890e146654341fb7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-28 15:22:04 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/6. Comparison Operator/4.greater_greaterEqual.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+	private: string name;
+	private: int moneyAmount;
+
+	public: Person()
+	{
+		name = "";
+		moneyAmount = 0;
+	}
+
+	public: Person(string name, int moneyAmount)
+	{
+		this -> name = name;
+		this -> moneyAmount = moneyAmount;
+	}
+
+	friend bool operator== (const Person &sakib, const Person &imran);
+	friend bool operator!= (const Person &sakib, const Person &imran);
+
+	friend bool operator> (const Person &sakib, const Person &imran);
+	friend bool operator>= (const Person &sakib, const Person &imran);
+
+	friend bool operator< (const Person &sakib, const Person &imran);
+	friend bool operator<= (const Person &sakib, const Person &imran);
+};
+
+bool operator== (const Person &sakib, const Person &imran)
+{
+	// if(moneyAmount == imran.moneyAmount)
+	// {
+	// 	return true;
+	// }
+	// else
+	// {
+	// 	return false;
+	// }
+
+	return (sakib.moneyAmount == imran.moneyAmount);
+}
+
+bool operator!= (const Person &sakib, const Person &imran)
+{
+	return !(sakib == imran);
+}
+
+int main(int argc, char const *argv[])
+{
+	Person linkon("linkon",150);
+	Person munir("munir",150);
+	Person nayeem("nayeem", 200);
+
+	if (linkon == munir)
+	{
+		cout << "linkon and munir has same amount of money" << endl;
+	}
+
+	if (linkon != nayeem)
+	{
+		cout << "linkon and nayeem do not has same amount of money" << endl;		
+	}
+
+	return 0;
+}
