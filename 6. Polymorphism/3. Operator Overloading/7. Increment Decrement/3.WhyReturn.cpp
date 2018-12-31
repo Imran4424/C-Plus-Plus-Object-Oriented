6453ef85b43da09a71a80e827df6606a061675a3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-31 09:35:29 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/7. Increment Decrement/3.WhyReturn.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Chocolate
+{
+	private: int countChocolate;
+
+	public: Chocolate()
+	{
+		countChocolate = 0;
+	}
+
+	public: Chocolate(int count)
+	{
+		countChocolate = count;
+	}
+
+	public: Chocolate& operator++()
+	{
+		++countChocolate;
+
+		return *this; 
+	}
+
+	public: Chocolate& operator--()
+	{
+		--countChocolate;
+
+		return *this; // notice we are returning the pointer
+	}
+
+	friend ostream& operator<<(ostream &imran, const Chocolate &sakib);
+};
+
+
+ostream& operator<<(ostream &imran, const Chocolate &sakib)
+{
+	imran << sakib.countChocolate << endl;
+
+	return imran; // this "imran" is a osstream object, object name can be anything
+}
+
+int main(int argc, char const *argv[])
+{
+	Chocolate kitkat(15);
+
+	cout << ++kitkat;
+	cout << --kitkat;
+	cout << --kitkat;
+	cout << ++kitkat;
+	cout << --kitkat;
+	cout << --kitkat;
+	cout << --kitkat;
+
+	return 0;
+}
+
