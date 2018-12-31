ca50bda30af62b256d9c86b944c280da2d43c565
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-31 09:26:33 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/7. Increment Decrement/2.Prefix_Decrement.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Chocolate
+{
+	private: int countChocolate;
+
+	public: Chocolate()
+	{
+		countChocolate = 0;
+	}
+
+	public: Chocolate(int count)
+	{
+		countChocolate = count;
+	}
+
+	public: Chocolate& operator++()
+	{
+		++countChocolate;
+
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream &imran, const Chocolate &sakib);
+};
+
+
+ostream& operator<<(ostream &imran, const Chocolate &sakib)
+{
+	imran << sakib.countChocolate << endl;
+
+	return imran; // this "imran" is a osstream object, object name can be anything
+}
+
+int main(int argc, char const *argv[])
+{
+	Chocolate kitkat(15);
+
+	cout << ++kitkat;
+	cout << ++kitkat;
+
+	return 0;
+}
+
