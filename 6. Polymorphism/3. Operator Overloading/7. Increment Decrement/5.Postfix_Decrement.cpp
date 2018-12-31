07b2a86f82571ec869d6d36641ca299f2bc8b59c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-31 10:03:03 +0600

    operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/7. Increment Decrement/5.Postfix_Decrement.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Chocolate
+{
+	private: int countChocolate;
+
+	public: Chocolate()
+	{
+		countChocolate = 0;
+	}
+
+	public: Chocolate(int count)
+	{
+		countChocolate = count;
+	}
+
+	public: Chocolate& operator++()
+	{
+		++countChocolate;
+
+		return *this; // notice we are returning the pointer
+	}
+
+	public: Chocolate& operator--()
+	{
+		--countChocolate;
+
+		return *this; // notice we are returning the pointer
+	}
+
+	public: Chocolate& operator++(int)
+	{
+		Chocolate temp(countChocolate);
+
+		++(*this); // use prefix operator to this pointer
+
+		return temp; // return the save value
+	}
+
+	// public: Chocolate& operator--(int)
+	// {
+	// 	Chocolate temp(countChocolate);
+
+	// 	--(*this); // use prefix operator to this pointer
+
+	// 	return temp; // return the save value
+	// }
+
+	friend ostream& operator<<(ostream &imran, const Chocolate &sakib);
+};
+
+
+ostream& operator<<(ostream &imran, const Chocolate &sakib)
+{
+	imran << sakib.countChocolate << endl;
+
+	return imran; // this "imran" is a osstream object, object name can be anything
+}
+
+int main(int argc, char const *argv[])
+{
+	Chocolate kitkat(15);
+
+	cout << ++kitkat;
+	cout << --kitkat;
+	cout << --kitkat;
+	cout << ++kitkat;
+	cout << --kitkat;
+	cout << --kitkat;
+
+	return 0;
+}
+
