6dd31c580aefb5ecd1e107f3b7042222dd80471a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-01 10:45:58 +0600

    simple blog

---
+++ 6. Polymorphism/3. Operator Overloading/8. Subscript Operator/3.Solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Game
+{
+	private: int levelEnemies[15];
+
+	public: Game()
+	{
+		//levelEnemies = {0}; this line will give error
+
+		for (int i = 0; i < 15; ++i)
+		{
+			levelEnemies[i] = 0;
+		}
+	}
+
+	public: void Set(int index,int val)
+	{
+		levelEnemies[index] = val;
+	}
+
+	public: int Get(int index)
+	{
+		return levelEnemies[index];
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Game red;
+
+	red.Set(5, 6);
+
+	cout << red.Get(5) << endl;
+
+
+	return 0;
+}
