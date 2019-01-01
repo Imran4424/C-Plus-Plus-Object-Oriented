144c15e872cb0b565bd5ace39bc2b5c2200404db
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-01 09:27:32 +0600

    subscript operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/8. Subscript Operator/2.Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Game
+{
+	private: int levelEnemies[15];
+
+	public: Game()
+	{
+		levelEnemies = {0};
+	}
+
+
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
