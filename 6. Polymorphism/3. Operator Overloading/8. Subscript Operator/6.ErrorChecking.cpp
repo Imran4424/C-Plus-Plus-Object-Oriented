7d918818d9dec4328b5be9c72fb2ae433f45abb2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-01 11:35:37 +0600

    subscript operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/8. Subscript Operator/6.ErrorChecking.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Game
+{
+	private: int levelEnemies[15];
+
+	public: Game()
+	{
+		//levelEnemies = {0}; this line will give error
+
+		for (int i = 0; i < 15; ++i)
+		{
+			levelEnemies[i] = 0;
+		}
+	}
+
+	public: int& operator[](const int index)
+	{
+		assert(index >= 0 && index < 15)
+
+		return levelEnemies[index];
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	Game red;
+
+	red[5] = 15;
+	cout << red[5] << endl;
+
+
+	return 0;
+}
