2b1ea58d38680ffce78931e06ff9aee034388350
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-02 16:45:12 +0600

    Assignment operator overloading

---
+++ 6. Polymorphism/3. Operator Overloading/11. Assignment/1.Assignment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+	private: int length, width, height;
+
+	public: Box(int length = 0; int width = 0; int height = 0)
+	{
+		this -> length = length;
+		this -> width = width;
+		this -> height = height;
+	}
+};
