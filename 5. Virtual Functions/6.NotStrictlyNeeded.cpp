92a44561901cef6b97fc98fad920464b568a3f3b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-03 17:07:10 +0600

    virtual function

---
+++ 5. Virtual Functions/6.NotStrictlyNeeded.cpp
@@ -0,0 +1,113 @@
+/*
+	Write a program to demonstrate use of pointers
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Engineer
+{
+	public: string profession;
+
+	public: Engineer()
+	{
+		profession = "";
+	}
+
+	public: Engineer(string profession)
+	{
+		this -> profession = profession;
+	}
+
+
+	public: virtual string GetJob()
+	{
+		return "do nothing";
+	}
+
+	public: string GetProfession()
+	{
+		return profession;
+	}
+};
+
+class Computer: public Engineer
+{
+	public: Computer(): Engineer()
+	{
+
+	}
+
+	public: Computer(string profession): Engineer(profession)
+	{
+
+	}
+
+	public: virtual string GetJob()
+	{
+		return "Develop Software";
+	}
+};
+
+
+class Civil: public Engineer
+{
+	public: Civil(): Engineer()
+	{
+
+	}
+
+	public: Civil(string profession): Engineer(profession)
+	{
+
+	}
+
+	public: virtual string GetJob()
+	{
+		return "Buid Building";
+	}
+};
+
+
+class Mechanical: public Engineer
+{
+	public: Mechanical(): Engineer()
+	{
+
+	}
+
+	public: Mechanical(string profession): Engineer(profession)
+	{
+
+	}
+
+	public: virtual string GetJob()
+	{
+		return "Work with Machinery";
+	}
+};
+
+
+void Report(Engineer *eng)
+{
+	cout << eng -> GetProfession() << " Engineers " << eng -> GetJob() << endl;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	Computer dipu("CSE");
+
+	Civil sadman("Civil");
+
+	Mechanical jannat("Mechanical");
+
+
+	Report(&dipu);
+	Report(&sadman);
+	Report(&jannat);
+
+	return 0;
+}
