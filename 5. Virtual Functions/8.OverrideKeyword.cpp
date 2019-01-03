b666889018cbbe7a865ab1c0bb1244adba3baca8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-03 17:49:38 +0600

    virtual functions

--- 5. Virtual Functions/8.OverrideKeyword.cpp
+++ 5. Virtual Functions/8.OverrideKeyword.cpp
@@ -35,15 +35,3 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
-
-/*
-	Do not call virtual functions from constructors or destructors
-
-	Downside
-
-	Virtual function is not efficient,resolving a virtual function call takes longer than resolving 
-	a regular one.
-
-	the compiler also has to allocate an extra pointer for each class object that has one or more 
-	virtual functions
-*/
