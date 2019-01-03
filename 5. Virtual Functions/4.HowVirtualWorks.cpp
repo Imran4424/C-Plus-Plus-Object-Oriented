172ea9d94bb873cece0995178faaad203e94e6a0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-03 16:56:12 +0600

    virtual function

--- 5. Virtual Functions/4.HowVirtualWorks.cpp
+++ 5. Virtual Functions/4.HowVirtualWorks.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
 
+class Base
+{
+	public: string GetMessage()
+	{
+		return "Base class function";
+	}
+};
+
+
+class Derived: public Base
+{
+	public: string GetMessage()
+	{
+		return "Derived from Base class function";
+	}
+};
+
+
+class DoubleDerived: public Derived
+{
+	public: string GetMessage()
+	{
+		return "Derived from Derived class function - Derived level Two";
+	}
+};
+
+
+class TripleDerived: public DoubleDerived
+{
+	public: string GetMessage()
+	{
+		return "Derived from Derived Derived class function - Derived level three";
+	}
+};
+
+
+
+
 int main(int argc, char const *argv[])
 {
 	/* code */
