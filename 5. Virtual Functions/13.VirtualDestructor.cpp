207c24892e694f1526168ed2048e0792216547eb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-04 15:08:28 +0600

    virtual functions

--- 5. Virtual Functions/13.VirtualDestructor.cpp
+++ 5. Virtual Functions/13.VirtualDestructor.cpp
@@ -27,10 +27,10 @@ class Derived: public Base
 
 int main(int argc, char const *argv[])
 {
-	//Derived red(15);
+	
 	Derived *red = new Derived(15);
 
-	Base *ptr = &red;
+	Base *ptr = red;
 
 	delete ptr;
 
