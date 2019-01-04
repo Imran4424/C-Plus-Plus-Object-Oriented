f156a51d41777dec0d07364fab2a08d81fdd73a8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-04 18:03:54 +0600

    virtual functions

---
+++ 5. Virtual Functions/19.EarlyBinding.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int Add(int x, int y)
+{
+	return x+y;
+}
+
+int Sub(int x, int y)
+{
+	return x-y;
+}
+
+int Mul(int x, int y)
+{
+	return x*y;
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << Add(7, 12) << endl;
+	
+	return 0;
+}
