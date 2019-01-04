f5a1cc84950dbba41dfe31b530bfc0c34ba1540e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-04 18:06:54 +0600

    virtual functions

--- 5. Virtual Functions/18.EarlyBinding.cpp
+++ 5. Virtual Functions/18.EarlyBinding.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int Add(int x, int y)
 {
-	return x+y;
+	return x + y;
 }
 
 int main(int argc, char const *argv[])
 {
 	cout << Add(7, 12) << endl;
-	
+
 	return 0;
 }
--- 5. Virtual Functions/20.EarlyBinding.cpp
+++ 5. Virtual Functions/20.EarlyBinding.cpp
@@ -18,6 +18,12 @@ int Mul(int x, int y)
 
 int main(int argc, char const *argv[])
 {
+	int choose;
+
+	cin >> choose;
+
+
+
 	cout << Add(7, 12) << endl;
 	
 	cout << Sub(7, 12) << endl;
