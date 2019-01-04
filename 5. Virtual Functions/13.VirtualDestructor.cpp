9e9e0ccf6e653f07fec431d0520b36b40d1a6674
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-04 15:03:24 +0600

    virtual functions

--- 5. Virtual Functions/13.VirtualDestructor.cpp
+++ 5. Virtual Functions/13.VirtualDestructor.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 
+class Base
+{
+	~Base()
+	{
+		cout << calling from Base Destructor << endl;
+	}
+};
+
+class Derived: public Base
+{
+	private: int *nothing;
+
+	Derived(int size)
+	{
+		nothing = new int[size];
+	}
+
+	~Derived()
+	{
+		delete[] nothing;
+		cout << calling from Derived Destructor << endl;
+	}
+};
+
 int main(int argc, char const *argv[])
 {
 	
 
-	
+
 	return 0;
 }
