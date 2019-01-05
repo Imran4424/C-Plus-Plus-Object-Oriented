fb4a60e10cff366dbaaee9b2bf74a73b665e5efd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-05 17:00:50 +0600

    templates - generic programming

--- 13. Templates/2.Problem.cpp
+++ 13. Templates/2.Problem.cpp
@@ -16,6 +16,8 @@ int main(int argc, char const *argv[])
 	cout << Add(4.5, 7.5) << endl;
 	
 	cout << Add(8.2, 12.36) << endl;
+
+	cout << Add(8.2, 12) << endl;
 	
 	cout << Add(string("Hi, "), string("Let's Play")) << endl;
 
