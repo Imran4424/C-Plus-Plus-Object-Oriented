323d063902bc16974a970bdf2f3a2c350f188cea
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-05 17:11:31 +0600

    templates - generic programming

---
+++ 13. Templates/2.DifferentType.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template <typename imran , typename sakib>
+imran Add(imran x, sakib y)
+{
+	return x + y;
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+	cout << Add(8, 12) << endl;
+	
+	cout << Add(4.5, 7.5) << endl;
+	
+	cout << Add(8.2, 12.36) << endl;
+
+	cout << Add(8.2, 12) << endl;
+	
+	cout << Add(string("Hi, "), string("Let's Play")) << endl;
+
+	return 0;
+}
