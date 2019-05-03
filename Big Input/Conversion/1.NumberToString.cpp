18be387f5e2b16b176ace3a701ede00bb185f660
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-03 07:55:22 +0600

    big inputs

--- Big Input/Conversion/1.NumberToString.cpp
+++ Big Input/Conversion/1.NumberToString.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	
+	int num = 12357667;
+
+	ostringstream word;
+
+	word << num;
+
+	string sNum = word.str();
+
+	cout << "The string from num is: " << sNum << endl;
+
 	return 0;
 }
