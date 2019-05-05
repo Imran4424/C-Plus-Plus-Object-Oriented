c86b7a1ff3daa5cc37686fa501935ec366eae988
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-05 17:00:50 +0600

    big input

--- Big Input/Conversion/3.StringToNumber.cpp
+++ Big Input/Conversion/3.StringToNumber.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
+	string s = "13579";
+
+	stringstream medium(s);
+
+	int num = 0;
+
+	medium >> num;
+
+	cout << "From string the number we get: " 
+
 	return 0;
 }
