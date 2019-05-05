12081e79eded4907b34f25d36aa0259a57eeeef0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-05 17:45:22 +0600

    big input

--- Big Input/Conversion/5.StringToNumber.cpp
+++ Big Input/Conversion/5.StringToNumber.cpp
@@ -8,7 +8,11 @@ int main(int argc, char const *argv[])
 	string str2 = "3.1416";
 	string str = "12 is even";
 
+	int num = stoi(str);
+
+	int num2 = stoi(str2);
 	
+	int num3 = stoi(str3);
 
 
 	return 0;
