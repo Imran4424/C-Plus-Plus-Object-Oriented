4a04fd178dfb70d42320dba211dd7be553b88352
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-05 16:50:52 +0600

    big input

--- Big Input/Conversion/2.NumberToString.cpp
+++ Big Input/Conversion/2.NumberToString.cpp
@@ -8,7 +8,13 @@ int main(int argc, char const *argv[])
 
 	double PI = 3.1416;
 
-	
+	string intWord = to_string(num);
+
+	string doubleWord = to_string(PI);
+
+	cout << "Interger string form is: " << intWord << endl;
+
+	cout << "Double string form is: " << doubleWord << endl;
 
 	return 0;
 }
---
+++ Big Input/Conversion/3.StringToNumber.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
