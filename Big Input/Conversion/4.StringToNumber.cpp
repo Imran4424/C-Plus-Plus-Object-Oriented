02df535e7ee5a23d47d56937408294c2f7865e9f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-05-05 17:19:11 +0600

    big input

--- Big Input/Conversion/4.StringToNumber.cpp
+++ Big Input/Conversion/4.StringToNumber.cpp
@@ -3,7 +3,21 @@
 
 int main(int argc, char const *argv[])
 {
+	char str = "951753";
+	char str2 = "3.1416";
+	char str3 = "13 is my id";
+
+	int num = atoi(str);
+
+	int num2 = atoi(str2);
 	
+	int num3 = atoi(str3);
+
+	cout << "atoi(\""951753"\") is: " << num << endl;
+
+	cout << "atoi(\""3.1416"\") is: " << num << endl;
 	
+	cout << "atoi(\""13 is my id"\") is: " << num << endl;
+
 	return 0;
 }
