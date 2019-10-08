e19b222705cbd225b22790da5655b23960bde4ba
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-08 09:44:09 +0600

    practice

--- 2. Class and Objects/1.Structre.cpp
+++ 2. Class and Objects/1.Structre.cpp
@@ -30,12 +30,12 @@ int main(int argc, char const *argv[])
 
 
 /*
-	In this chapter we will learn about class and objects
+	In this chapter we will learn about class and objects.
 
-	for learning classes first we need to understand the basic operations of structres
+	for learning classes first we need to understand the basic operations of structres.
 
 	although classes are different from structure, the low level basic concept of 
-	classes are same as structures
+	classes are same as structures.
 
 
 	because classes are build on the basic concept of structures and evolve into a new 
@@ -45,7 +45,7 @@ int main(int argc, char const *argv[])
 	now, let's talk about struct
 
 	structs are mainly a package of variables and functions, it is also called as user 
-	defined data types
+	defined data types.
 
 	in a struct code block we can declare multiple variables and functions in a single 
 	code block as for the requirements and access them using struct type variable
@@ -60,7 +60,8 @@ int main(int argc, char const *argv[])
 
 	struct Student
 	{
-		int id;
+		// variables
+		int id; 
 		string name;
 
 		Student() { } // default constructor
@@ -70,10 +71,18 @@ int main(int argc, char const *argv[])
 			id = sId;
 			name = sName;
 		}
+
+		//functions
+
+		void Display()
+		{
+			cout << id << " " << name << endl;
+		}
 	};
 
-	now, you may ask what is constructor ? for now just remember constructor are helpful in
-	creating objects. Later we will have a long discuss about constuctor
+	now, you may ask what is constructor ? for now just remember constructor are 
+	helpful in structure type variables. Later we will have a long discuss about 
+	constuctor
 
 	see the above code for better understanding
 */
