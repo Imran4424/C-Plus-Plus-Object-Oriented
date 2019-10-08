6e545bf7fa5733cd1d268ef925d803ad80c53a9b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-09 01:59:10 +0600

    pointers

---
+++ Pointers/3.PointerDeclaration.cpp
@@ -0,0 +1,27 @@
+/*
+	In this code we will learn how do declare pointer variables
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	int *ptr;
+	return 0;
+}
+
+
+	* - now what is this
+	* - is this dereference? No. Then, what is it?
+	* - this time it is asterisk
+
+	I know what you are feeling now. Cause same operator has three names
+
+	* - multipication , binary
+	* - dereference , unary
+	* - asterisk , unary
+
+	
