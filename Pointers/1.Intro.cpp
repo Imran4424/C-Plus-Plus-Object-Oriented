5425cc953310d17432e4ea594b39eccc8d11b309
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-09 00:31:56 +0600

    pointers

---
+++ Pointers/1.Intro.cpp
@@ -0,0 +1,27 @@
+/*
+	In this code we will have a introduction of pointers
+
+	basic programming knowledge required
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
+
+/*
+	As we know variable name is a piece of memory that holds a value (array is 
+	collection of sequential of memories)
+
+	memory size differs according to the data type
+
+	char - 1 byte 
+	int - 2 or 4 byte (depends on the compiler)
+	float - 4 byte
+	double - 8 byte
+	
+*/
