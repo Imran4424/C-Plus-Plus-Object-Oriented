9d787a6ead1454c0dbe3af455b3476cc072de7dd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-11 07:53:24 +0600

    pointers

--- Pointers/7.PointerArithematic.cpp
+++ Pointers/7.PointerArithematic.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	int a = 10;
+
+	int *ptr = &a;
+
+	/*
+		this way is also fine
+
+		int *ptr;
+		ptr = &a;
+	*/
+
+	cout << a << endl; 
+	cout << a + 1 << endl; 
+
+	cout << dec << a << endl; 
+	cout << dec << a + 1 << endl; 
+
 	
 	return 0;
 }
