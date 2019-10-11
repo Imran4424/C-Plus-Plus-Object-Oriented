a1d39cb2800c0444e0a9dbc3239620d27870b167
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-11 06:55:30 +0600

    pointers

--- Pointers/4.VariablePointing.cpp
+++ Pointers/4.VariablePointing.cpp
@@ -12,10 +12,17 @@ int main(int argc, char const *argv[])
 	int a, b;
 
 	cout << "enter a and b" << endl;
-	
+
 	cin >> a >> b;
 
 	int *ptr = &a;
+
+	cout << "normal variable: " << a << endl;
+	cout << "variable address: "<< &a << endl;
+
+	cout << "pointer variable value: " << ptr << endl;
+	cout << "pointer variable value: " << *ptr << endl;
+
 
 	return 0;
 }
