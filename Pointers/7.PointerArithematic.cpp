2814f81478890cbeb3f8893319c9b9037aeeab61
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-11 07:59:13 +0600

    pointers

--- Pointers/7.PointerArithematic.cpp
+++ Pointers/7.PointerArithematic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stio.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -17,9 +18,11 @@ int main(int argc, char const *argv[])
 	cout << ptr << endl; 
 	cout << ptr + 1 << endl; 
 
-	cout << dec << ptr << endl; 
-	cout << dec << ptr + 1 << endl; 
+	// cout << dec << ptr << endl; 
+	// cout << dec << ptr + 1 << endl; 
 
+	printf("%d\n", ptr);
+	printf("%d\n", ptr + 1);
 	
 	return 0;
 }
