95f338cbbda86b03f1d48e9ea927d11f2d870b86
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-11 23:40:25 +0600

    pointers

--- Pointers/7.PointerArithematic.cpp
+++ Pointers/7.PointerArithematic.cpp
@@ -16,7 +16,27 @@ int main(int argc, char const *argv[])
 	*/
 
 	printf("%d\n", iPtr);
+	printf("size of integer is %d bytes\n", sizeof(int));
 	printf("%d\n", iPtr + 1);
+
+
+	char ch = 'X';
+
+	char *cPtr;
+	cPtr = &ch;
+
+	printf("%d\n", cPtr);
+	printf("size of char is: %d bytes\n", sizeof(char));
+	printf("%d\n", cPtr + 1);
+
+
+	double pi = 3.1416;
+
+	double* dPtr = &pi;
+
+	printf("%d\n", dPtr);
+	printf("size of double is: %d bytes\n", sizeof(double));
+	printf("%d\n", dPtr + 1);
 	
 	return 0;
 }
