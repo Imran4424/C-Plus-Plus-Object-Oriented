5e96103705894f837fa11bf7c6d09410fecf3617
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-12 16:12:35 +0600

    pointers

--- Pointers/SmartPoiner.cpp
+++ Pointers/SmartPoiner.cpp
@@ -2,16 +2,9 @@
 helpful links:
 https://www.internalpointers.com/post/beginner-s-look-smart-pointers-modern-c
 
-C++11 has introduced three types of smart pointers, all of them defined in 
-the <memory> header from the Standard Library:
-
-    std::unique_ptr — a smart pointer that owns a dynamically allocated resource;
-
-    std::shared_ptr — a smart pointer that owns a shared dynamically allocated 
-    resource. Several std::shared_ptrs may own the same resource and an internal 
-    counter keeps track of them;
-    
-    std::weak_ptr — like a std::shared_ptr, but it doesn't increment the counter.
-
+A std::unique_ptr owns of the object it points to and no other smart pointers 
+can point to it. When the std::unique_ptr goes out of scope, the object is deleted. 
+This is useful when you are working with a temporary, dynamically-allocated 
+resource that can get destroyed once out of scope.
 
 */
