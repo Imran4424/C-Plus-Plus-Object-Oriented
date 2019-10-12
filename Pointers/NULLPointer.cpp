ce245d15a444b9364b1f1653438d061c257d72f0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-12 15:33:57 +0600

    pointers

--- Pointers/NULLPointer.cpp
+++ Pointers/NULLPointer.cpp
@@ -3,6 +3,41 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	double *ptr {0}; // null pointer
+	/*
+		this is also valid
 	
+		double *ptr = 0;
+	*/
+
+	int *ptrTwo;
+	ptrTwo = 0;
+
+	int *ptrThree {NULL};
+
+	/*
+		this is also valid
+	
+		int *ptrThree = NULL;
+	*/
+
+	int *ptrFour;
+	ptrFour = NULL;
+
+	int *ptrFive { nullptr};
+
+	/*
+		this is also valid
+	
+		int *ptrFive = nullptr;
+	*/
+
+	int *ptrSix;
+	ptrSix = nullptr;
+
+	int *pointer = NULL;
+
+	cout << pointer << endl;
+
 	return 0;
 }
