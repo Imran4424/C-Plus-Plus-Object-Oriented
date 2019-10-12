42c7af47ec52ab5aaeb118a90547796a0ab7f414
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-12 08:35:26 +0600

    pointers

--- Pointers/8.PointerVariableType.cpp
+++ Pointers/8.PointerVariableType.cpp
@@ -13,3 +13,16 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+
+/*
+	Pointer variables are strong type
+
+	why pointer variables are strong type? cause it only storing variable address
+	why not generic type?
+
+	we do not use pointer variables only to store memory addresses but we also use them
+	to dereference these addresses so that we can access and modify values in this
+	addresses.
+	And not only different data type sizes are different but also their value
+	representation is also different.
+*/
