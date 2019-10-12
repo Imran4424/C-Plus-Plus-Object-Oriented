360bab5e9d5ddde98345f6c94069a6f5c2d3b888
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-12 17:13:47 +0600

    pointers

--- Pointers/SmartPoiner.cpp
+++ Pointers/SmartPoiner.cpp
@@ -7,4 +7,9 @@ can point to it. When the std::unique_ptr goes out of scope, the object is delet
 This is useful when you are working with a temporary, dynamically-allocated 
 resource that can get destroyed once out of scope.
 
+std::unique_ptr<int>    p1 = std::make_unique<int>();
+std::unique_ptr<int[]>  p2 = std::make_unique<int[]>(50);
+std::unique_ptr<Object> p3 = std::make_unique<Object>("Lamp");
+
+
 */
