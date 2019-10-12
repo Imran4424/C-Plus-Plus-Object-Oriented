781360f1df4b6b8473c166998a09695c7c63194a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-12 14:10:22 +0600

    pointers

--- Pointers/ThisPointerStructure.cpp
+++ Pointers/ThisPointerStructure.cpp
@@ -35,6 +35,19 @@ struct box
 
 int main(int argc, char const *argv[])
 {
-	
+	box red;
+
+	red.Assign(10, 5, 8);
+
+	cout << "Red box  Area is: " << red.Area() << endl;
+	cout << "Red box  Volume is: " << red.Volume() << endl;
+
+	cout << endl ;
+
+	box green(20, 10, 16);
+
+	cout << "green box Area is:" << green.Area() << endl;
+	cout << "green box Volume is:" << green.Volume() << endl;
+
 	return 0;
 }
