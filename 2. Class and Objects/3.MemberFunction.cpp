c10f4bdb66965c5dc87b9be70030df16467d43f6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-15 00:39:10 +0600

    class & object

--- 2. Class and Objects/3.MemberFunction.cpp
+++ 2. Class and Objects/3.MemberFunction.cpp
@@ -73,6 +73,65 @@ int main(int argc, char const *argv[])
 	Advantage - executes faster because compiler doesn't have to deal with overheads of 
 	            searching the function body int the program
 
-	Disadvantage - inside functions(inline functions) take up more memory because 
+	Disadvantage - inside functions(inline functions) take up more memory because they are copied
+	               into every call instead of being defined just once.
 
+
+	
+	Outside Class members declared inside the class but their body defined outside the class
+
+	class Box
+	{
+	
+		private: int length, width;
+
+		// Member function
+		// inside the class
+		void Assign(int l, int w) 
+		{
+			length = l;
+			width = w;
+		}
+		
+		int Area(); // just declaration
+	};
+
+	// Member function
+	// Outside the class
+	// Definition with body
+
+	int Box::Area()
+	{
+		return width*length;
+	}
+
+
+	Advantage - Takes very less memory memory because function is just defined once for every
+	            function call
+
+	Disadvantage - Takes more runtime than inside class function because compiler needs to
+	               find the function body then jump the control to the function body after 
+	               finishing the function compiler jump back the control to the class. These
+	               overhead increase the run time.
+
+	
+	Considerations
+
+	For small and simple member functions we can easily define members function inside the class
+
+	But
+
+	For large and complex function body we should use outside class definition for member
+	functions
+
+	Note
+
+	:: - this is an operator, this is called scope resolution
+
+	this operator is used after the class and before the class member.
+	This is a binary operator
+
+	This operator is also used to define outside class definition for member functions
+
+	
 */
