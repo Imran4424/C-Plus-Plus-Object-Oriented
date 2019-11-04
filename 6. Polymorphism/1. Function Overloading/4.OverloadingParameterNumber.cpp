3eb457c8c76b6833a82271b84cf62749d99796cf
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-05 02:31:29 +0600

    function overloading

--- 6. Polymorphism/1. Function Overloading/4.OverloadingParameterNumber.cpp
+++ 6. Polymorphism/1. Function Overloading/4.OverloadingParameterNumber.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int Add(int a, int b)
+{
+	return a + b;
+}
 
+int Add(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+int Add(int a, int b, int c, int d)
+{
+	return a + b + c + d;
+}
 
 int main(int argc, char const *argv[])
 {
