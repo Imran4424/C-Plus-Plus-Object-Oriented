100e3293f39aa4a74118b0484a281e8289240f34
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-05 02:53:04 +0600

    function overloading

--- 6. Polymorphism/1. Function Overloading/7.DefaultParameters.cpp
+++ 6. Polymorphism/1. Function Overloading/7.DefaultParameters.cpp
@@ -11,6 +11,11 @@ double Add(double a = 0, double b = 0, double c = 0, double d = 0)
 	return a + b + c + d;
 }
 
+double Add(double a = 0, int b = 0, int c = 0, double d = 0)
+{
+	return a + b + c + d;
+}
+
 int main(int argc, char const *argv[])
 {
 	
