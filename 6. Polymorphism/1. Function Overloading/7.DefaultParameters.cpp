6b5a8489eec7a3f45c33ae1ad62ea879a280353e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-05 02:50:58 +0600

    function overloading

---
+++ 6. Polymorphism/1. Function Overloading/7.DefaultParameters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int Add(int a = 0, int b = 0, int c = 0, int d = 0)
+{
+	return a + b + c + d;
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
