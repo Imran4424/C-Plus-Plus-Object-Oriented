37ca1239934e2673c7cc1be87f6b506db14a617d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-14 00:09:59 +0600

    string

--- 8. C++ String Class/WordReverse.cpp
+++ 8. C++ String Class/WordReverse.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	char givenStr[100];
+	string givenStr;
+	getline(cin, givenStr);
 
-	scanf("%[^\n]%*c", givenStr);
+	vector <string> words;
+
+	string tempWord;
+
+	for(int i = 0; i <= givenStr.size(); i++) {
+	
+		if(' ' == givenStr[i] || i == givenStr.size()) {
+			words.push_back(tempWord);
+
+			tempWord.clear();
+
+			continue;
+		}
+
+		tempWord += givenStr[i];	
+	}
+
+	string reveseStr;
+
+	for(int i = words.size() - 1; i >= 0; i--) {
+
+		reveseStr += words[i];
+	}
+
+	cout << reveseStr << endl;
 
 	return 0;
 }
