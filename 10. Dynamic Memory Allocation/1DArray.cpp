c14a116c7d57a4a50ccc5fec97633ef8c53218b8
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-15 23:17:12 +0600

    dynamic memory allocation

--- 10. Dynamic Memory Allocation/1DArray.cpp
+++ 10. Dynamic Memory Allocation/1DArray.cpp
@@ -9,11 +9,28 @@ void initArray(int size) {
 	arr = new int[size];
 }
 
+void populateArray(int size) {
+	for(int i = 0; i < size; i++) {
+		scanf("%d", &arr[i]);
+	}
+}
+
+void displayArray(int size) {
+	for(int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
 int main(int argc, char const *argv[])
 {
 	int size;
 	scanf("%d", size);
+	
 	initArray(size);
+	populateArray(size);
+	displayArray(size);
 
 	return 0;
 }
