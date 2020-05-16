11fd21afc86a746d3e711f7ec835d7a681655773
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-16 11:10:25 +0600

    dynamic memory allocation

--- 10. Dynamic Memory Allocation/2DArray.cpp
+++ 10. Dynamic Memory Allocation/2DArray.cpp
@@ -2,11 +2,18 @@
 #include <stdio.h>
 using namespace std;
 
-
+// declaring pointer of pointer for 2D Array
 int **grid;
+int row, col;
+
+void initGrid(int row, int col) {
+	
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	row = 3;
+	col = 4;
+
 	return 0;
 }
