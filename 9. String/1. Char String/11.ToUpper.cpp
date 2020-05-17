97472af354926c3ff95fc87924f21161e83d858e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 15:22:36 +0600

    char string

--- 9. String/1. Char String/11.ToUpper.cpp
+++ 9. String/1. Char String/11.ToUpper.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void toUpper(char *str) {
 	for(int i = 0; str[i] != '\0'; i++) {
-		if()
-
-		str[i] = str[i] - 32;
+		if(str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] - 32;
+		}
 	}
 }
 
