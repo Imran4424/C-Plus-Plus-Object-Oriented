4bfec1758af4132b481498afddfb7ad7b5137f3f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 15:23:36 +0600

    char string

--- 9. String/1. Char String/10.ToLower.cpp
+++ 9. String/1. Char String/10.ToLower.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 void toLower(char *str) {
 	for(int i = 0; str[i] != '\0'; i++) {
-		str[i] = str[i] + 32;
+		if(str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] + 32;
+		}
 	}
 }
 
