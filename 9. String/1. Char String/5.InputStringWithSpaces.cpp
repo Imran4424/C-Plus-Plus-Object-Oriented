e836e1a2bf6adc50341d3dd0f3798bfbfcba2e80
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 14:46:55 +0600

    char string

--- 9. String/1. Char String/5.InputStringWithSpaces.cpp
+++ 9. String/1. Char String/5.InputStringWithSpaces.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	
-	
+	char text[100];
+
+	cout << "enter a line" << endl;
+	cin.getline(text, sizeof(text));
+
+	cout << endl << "entered text is - " << endl;
+	cout << text << endl; 
+
 	return 0;
 }
