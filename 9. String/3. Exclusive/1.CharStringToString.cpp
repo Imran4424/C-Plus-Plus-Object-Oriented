c8ce2e9ca5f8b83ed840a9bae836359f2d2694fa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-27 21:44:47 +0600

    string

---
+++ 9. String/3. Exclusive/1.CharStringToString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// char string
+	char chStr = "I am a Programmer";
+
+	// c++ string
+	string text;
+
+	text = chStr;
+
+	cout << text << endl;
+
+	return 0;
+}
