b3bc433e7ef3ad587142961cca2bb70a6ac46a69
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2022-08-19 15:47:20 +0600

    virtual function

--- 5. Virtual Functions/1.Basic/8.OverrideKeyword.cpp
+++ 5. Virtual Functions/1.Basic/8.OverrideKeyword.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Base
 {
+	// conflicting return type
 	public: virtual int GetData()
 	{
 		return 13;
@@ -12,9 +13,14 @@ class Base
 
 class Derived: public Base
 {
+	// conflicting return type
 	public: virtual string GetData() override
 	{
 		return "I am a Programmer";
+	}
+
+	public: int justForTesting() override {
+		return 11;
 	}
 };
 
@@ -35,3 +41,18 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
+
+
+/*
+	This code will produce error
+
+	function overriding basic is function signature need to be same
+
+	here, which is not the case
+
+	override keyword is for visually distinguish overriding functions from normal functions
+
+	And 
+
+	override keyword also tells the compiler that, there is a base function of this class, otherwise it won't compile
+*/
